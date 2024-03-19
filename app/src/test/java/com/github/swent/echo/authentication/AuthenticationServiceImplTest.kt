bd2f327acd3ee7e68736fee2b7b6bb050de9a0be
octogradiste <colin.berger@outlook.com>
2024-03-19 22:29:47 +0100

    Testing the supabase authentication service

--- app/src/test/java/com/github/swent/echo/authentication/AuthenticationServiceImplTest.kt
+++ app/src/test/java/com/github/swent/echo/authentication/AuthenticationServiceImplTest.kt
@@ -80,7 +80,7 @@ class AuthenticationServiceImplTest {
     @Test
     fun `getCurrentUserID should return user id when user is signed in`() {
         val service = AuthenticationServiceImpl(authMock)
-        every { authMock.currentSessionOrNull() } returns
+        coEvery { authMock.currentSessionOrNull() } returns
             mockk { every { user } returns mockk { every { id } returns USER_ID } }
         val result = runBlocking { service.getCurrentUserID() }
         assertEquals(USER_ID, result)
