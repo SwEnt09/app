bb477207beab23fa6e9a64733517b48f5ffd30e9
octogradiste <colin.berger@outlook.com>
2024-03-28 16:42:48 +0100

    Use hilt to inject authentication service

--- app/src/main/java/com/github/swent/echo/viewmodels/authentication/LoginViewModel.kt
+++ app/src/main/java/com/github/swent/echo/viewmodels/authentication/LoginViewModel.kt
@@ -4,16 +4,19 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.swent.echo.authentication.AuthenticationResult
 import com.github.swent.echo.authentication.AuthenticationService
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 /**
  * ViewModel for the login screen.
  *
  * @param auth The authentication service to use.
  */
-class LoginViewModel(private val auth: AuthenticationService) : ViewModel() {
+@HiltViewModel
+class LoginViewModel @Inject constructor(private val auth: AuthenticationService) : ViewModel() {
 
     private val _state = MutableStateFlow<AuthenticationState>(AuthenticationState.SignedOut)
 
