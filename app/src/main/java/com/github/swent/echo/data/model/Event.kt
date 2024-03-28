852c47a107a39fafe267fafb571fecb6dcb80479
Jonas Sulzer <jonas@violoncello.ch>
2024-03-29 00:18:25 +0100

    👌 IMPROVE: use Date type for event start and end
    Signed-off-by: Jonas Sulzer <jonas@violoncello.ch>

--- app/src/main/java/com/github/swent/echo/data/model/Event.kt
+++ app/src/main/java/com/github/swent/echo/data/model/Event.kt
@@ -1,11 +1,13 @@
 package com.github.swent.echo.data.model
 
+import java.util.Date
+
 data class Event(
     val eventId: String,
     val organizerId: String,
     val title: String,
     val description: String,
     val location: String, // TODO: Change to Location object, once it is implemented
-    val startDate: String,
-    val endDate: String
+    val startDate: Date,
+    val endDate: Date
 )
