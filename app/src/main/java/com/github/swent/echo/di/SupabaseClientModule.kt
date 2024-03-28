7d8d747d8d6b84174843ced0b8c53f2ae0b8ec6d
octogradiste <colin.berger@outlook.com>
2024-03-28 23:30:28 +0100

    Installing authentication and database plugin for supabase

--- app/src/main/java/com/github/swent/echo/di/SupabaseClientModule.kt
+++ app/src/main/java/com/github/swent/echo/di/SupabaseClientModule.kt
@@ -6,6 +6,8 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.Auth
+import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.serializer.KotlinXSerializer
 import kotlinx.serialization.json.Json
 
@@ -20,6 +22,8 @@ class SupabaseClientModule {
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsZWpuaXZndXhlaWlia2Jwd25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4MzgxODQsImV4cCI6MjAyNjQxNDE4NH0.9Hkj-Gox2XHcHfs_U2GyQFc9sZ_nu2Xs16-KYBri32g"
         return createSupabaseClient(supabaseUrl, supabaseKey) {
             defaultSerializer = KotlinXSerializer(Json)
+            install(Auth)
+            install(Postgrest)
         }
     }
 }
