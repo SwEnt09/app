010449da905bb3acb8f9ffa369a6208a3f01ab7b
octogradiste <colin.berger@outlook.com>
2024-03-29 18:09:17 +0100

    Configure hilt test runner for android tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "com.github.swent.echo.hilt.HiltTestRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
