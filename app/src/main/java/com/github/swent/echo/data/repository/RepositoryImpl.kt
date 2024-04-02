a2555b3db9a5ce9d2a572dbdf8a8149c13cdc982
Jonas Sulzer <jonas@violoncello.ch>
2024-04-02 21:40:30 +0200

    📦 NEW: implement repository
    Signed-off-by: Jonas Sulzer <jonas@violoncello.ch>

---
+++ app/src/main/java/com/github/swent/echo/data/repository/RepositoryImpl.kt
@@ -0,0 +1,62 @@
+package com.github.swent.echo.data.repository
+
+import com.github.swent.echo.data.model.Association
+import com.github.swent.echo.data.model.Event
+import com.github.swent.echo.data.model.Tag
+import com.github.swent.echo.data.model.User
+import com.github.swent.echo.data.model.UserProfile
+import com.github.swent.echo.data.repository.datasources.RemoteDataSource
+
+class RepositoryImpl(private val remoteDataSource: RemoteDataSource) : Repository {
+    override suspend fun getAssociation(associationId: String): Association {
+        return remoteDataSource.getAssociation(associationId)
+    }
+
+    override suspend fun setAssociation(association: Association) {
+        return remoteDataSource.setAssociation(association)
+    }
+
+    override suspend fun getAllAssociations(): List<Association> {
+        return remoteDataSource.getAllAssociations()
+    }
+
+    override suspend fun getEvent(eventId: String): Event {
+        return remoteDataSource.getEvent(eventId)
+    }
+
+    override suspend fun setEvent(event: Event) {
+        return remoteDataSource.setEvent(event)
+    }
+
+    override suspend fun getAllEvents(): List<Event> {
+        return remoteDataSource.getAllEvents()
+    }
+
+    override suspend fun getTag(tagId: String): Tag {
+        return remoteDataSource.getTag(tagId)
+    }
+
+    override suspend fun setTag(tag: Tag) {
+        return remoteDataSource.setTag(tag)
+    }
+
+    override suspend fun getAllTags(): List<Tag> {
+        return remoteDataSource.getAllTags()
+    }
+
+    override suspend fun getUser(userId: String): User {
+        return remoteDataSource.getUser(userId)
+    }
+
+    override suspend fun setUser(user: User) {
+        return remoteDataSource.setUser(user)
+    }
+
+    override suspend fun getUserProfile(userId: String): UserProfile {
+        return remoteDataSource.getUserProfile(userId)
+    }
+
+    override suspend fun setUserProfile(userProfile: UserProfile) {
+        return remoteDataSource.setUserProfile(userProfile)
+    }
+}
