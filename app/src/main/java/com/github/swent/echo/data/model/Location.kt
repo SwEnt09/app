7ebad8866cb5e3bb3beaa13d576510af36249e2c
alejandrocalles <gabriel.jimenezcalles@epfl.ch>
2024-04-04 05:14:13 +0200

    Format code

--- app/src/main/java/com/github/swent/echo/data/model/Location.kt
+++ app/src/main/java/com/github/swent/echo/data/model/Location.kt
@@ -6,6 +6,7 @@ data class Location(val name: String, val lat: Double, val long: Double) {
     constructor(name: String, point: GeoPoint) : this(name, point.latitude, point.longitude)
     /**
      * Transform this location into a [GeoPoint].
+     *
      * @return The respective [GeoPoint]
      */
     fun toGeoPoint() = GeoPoint(lat, long)
