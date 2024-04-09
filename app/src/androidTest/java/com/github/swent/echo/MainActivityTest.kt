58554c4cceb69b25f3ca09c7f866d8564c7bbee6
octogradiste <colin.berger@outlook.com>
2024-04-09 20:00:35 +0200

    Add full registration process test

--- app/src/androidTest/java/com/github/swent/echo/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/swent/echo/MainActivityTest.kt
@@ -4,9 +4,9 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -17,11 +17,6 @@ class MainActivityTest {
 
     // Create a test rule for the `MainActivity`
     @get:Rule(order = 1) val composeTestRule = createAndroidComposeRule<MainActivity>()
-
-    @Before
-    fun setUp() {
-        hiltRule.inject()
-    }
 
     @Test
     fun shouldDisplayRegisterRouteWhenAppStarts() {
@@ -42,5 +37,26 @@ class MainActivityTest {
         // Navigate back to the register screen
         composeTestRule.onNodeWithTag("register-button").performClick()
         composeTestRule.onNodeWithTag("register-screen").assertIsDisplayed()
+    }
+
+    @Test
+    fun testRegistrationProcess() {
+        val email = "test@test.com"
+        val password = "password"
+
+        // Perform the registration process
+        composeTestRule.onNodeWithTag("register-screen").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("email-field").performTextInput(email)
+        composeTestRule.onNodeWithTag("password-field").performTextInput(password)
+        composeTestRule.onNodeWithTag("action-button").performClick()
+
+        // Login with the registered user
+        composeTestRule.onNodeWithTag("login-screen").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("email-field").performTextInput(email)
+        composeTestRule.onNodeWithTag("password-field").performTextInput(password)
+        composeTestRule.onNodeWithTag("action-button").performClick()
+
+        // The map screen should be displayed
+        composeTestRule.onNodeWithTag("mapScreen").assertIsDisplayed()
     }
 }
