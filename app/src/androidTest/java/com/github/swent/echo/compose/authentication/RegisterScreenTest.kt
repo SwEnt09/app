95920e60fe9261b4408e8610c164236e07ea987c
octogradiste <colin.berger@outlook.com>
2024-04-09 12:36:49 +0200

    Add tests for navigation between login and register screens

--- app/src/androidTest/java/com/github/swent/echo/compose/authentication/RegisterScreenTest.kt
+++ app/src/androidTest/java/com/github/swent/echo/compose/authentication/RegisterScreenTest.kt
@@ -6,6 +6,8 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
+import com.github.swent.echo.ui.navigation.NavigationActions
+import com.github.swent.echo.ui.navigation.Routes
 import com.github.swent.echo.viewmodels.authentication.AuthenticationState
 import com.github.swent.echo.viewmodels.authentication.RegisterViewModel
 import io.mockk.every
@@ -22,14 +24,16 @@ class RegisterScreenTest {
     @get:Rule val composeTestRule = createComposeRule()
 
     private lateinit var registerViewModel: RegisterViewModel
+    private lateinit var navActions: NavigationActions
     private lateinit var state: MutableStateFlow<AuthenticationState>
 
     @Before
     fun setUp() {
         registerViewModel = mockk(relaxed = true)
+        navActions = mockk(relaxed = true)
         state = MutableStateFlow(AuthenticationState.SignedOut)
         every { registerViewModel.state } answers { state.asStateFlow() }
-        composeTestRule.setContent { RegisterScreen(registerViewModel) }
+        composeTestRule.setContent { RegisterScreen(registerViewModel, navActions) }
     }
 
     @Test
@@ -54,5 +58,19 @@ class RegisterScreenTest {
         composeTestRule.onNodeWithTag("password-field").performTextInput("password")
         composeTestRule.onNodeWithTag("action-button").performClick()
         verify { registerViewModel.register("test@test.test", "password") }
+    }
+
+    @Test
+    fun shouldCallNavigateToLoginWhenLoginButtonIsClickedInSignedOutState() {
+        state.value = AuthenticationState.SignedOut
+        composeTestRule.onNodeWithTag("login-button").performClick()
+        verify { navActions.navigateTo(Routes.LOGIN) }
+    }
+
+    @Test
+    fun shouldCallNavigateToLoginWhenLoginButtonIsClickedInErrorState() {
+        state.value = AuthenticationState.Error("Error message")
+        composeTestRule.onNodeWithTag("login-button").performClick()
+        verify { navActions.navigateTo(Routes.LOGIN) }
     }
 }
