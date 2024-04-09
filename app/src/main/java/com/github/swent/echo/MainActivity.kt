c91ba242ffbfc23579a431d177a253c679e4969a
octogradiste <colin.berger@outlook.com>
2024-04-09 09:51:20 +0200

    Setting the AppNavigationHost as the main composable and removing Greeting

--- app/src/main/java/com/github/swent/echo/MainActivity.kt
+++ app/src/main/java/com/github/swent/echo/MainActivity.kt
@@ -6,11 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.github.swent.echo.compose.map.configureOsmdroid
+import com.github.swent.echo.compose.navigation.AppNavigationHost
 import com.github.swent.echo.ui.theme.EchoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,20 +24,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AppNavigationHost()
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EchoTheme { Greeting("Android") }
 }
