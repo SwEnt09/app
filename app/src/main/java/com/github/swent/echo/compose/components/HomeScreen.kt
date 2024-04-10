4c7f0165828192b3be85bc9a74ece4709df29283
Nathan Tissières <nathan.tissieres@epfl.ch>
2024-04-10 17:20:54 +0200

    Implementation of the hamburger menu in the top bar
    Replacing the light/dark mode button in hamburger menu by a close hamburger menu button
    
    Adding tests for hamburger menu and adapting the old ones

--- app/src/main/java/com/github/swent/echo/compose/components/HomeScreen.kt
+++ app/src/main/java/com/github/swent/echo/compose/components/HomeScreen.kt
@@ -3,6 +3,35 @@ package com.github.swent.echo.compose.components
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.AddCircle
+import androidx.compose.material.icons.outlined.Build
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.Face
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -50,16 +79,102 @@ fun HomeScreen(navActions: NavigationActions) {
     Scaffold(
         modifier = Modifier.testTag("home_screen"),
         topBar = {
-            TopBar(
-                {},
-                {
-                    if (mode.value == MapOrListMode.MAP) {
-                        mode.value = MapOrListMode.LIST
-                    } else {
-                        mode.value = MapOrListMode.MAP
-                    }
+            TopBar(navActions) {
+                if (mode.value == MapOrListMode.MAP) {
+                    mode.value = MapOrListMode.LIST
+                } else {
+                    mode.value = MapOrListMode.MAP
                 }
             )
+        )
+
+    // Hamburger menu compose
+    ModalNavigationDrawer(
+        // Content of the hamburger menu
+        drawerContent = {
+            ModalDrawerSheet {
+                // Profile picture, name and class
+                Box(
+                    modifier =
+                    Modifier.background(MaterialTheme.colorScheme.primaryContainer)
+                        .fillMaxWidth()
+                        .padding(NavigationDrawerItemDefaults.ItemPadding)
+                        .testTag("profile_box")
+                ) {
+                    Column(
+                        modifier =
+                        Modifier.align(Alignment.TopStart)
+                            .padding(8.dp)
+                            .testTag("profile_sheet")
+                    ) {
+                        // TODO: Replace with actual profile picture
+                        Image(
+                            modifier = Modifier.testTag("profile_picture"),
+                            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                            contentDescription = "profile picture"
+                        )
+                        Row(modifier = Modifier.padding(8.dp).testTag("profile_info")) {
+                            // TODO: Replace with actual name and class
+                            Text(
+                                text = "John Doe",
+                                modifier = Modifier.testTag("profile_name"),
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(
+                                text = "IN - BA6",
+                                color = Color.White.copy(alpha = 0.5f),
+                                modifier = Modifier.testTag("profile_class")
+                            )
+                        }
+                    }
+                    // Close button for the hamburger menu
+                    IconButton(
+                        onClick = {
+                            selectedItemIndex = 0
+                            scope.launch { drawerState.close() }
+                        },
+                        modifier =
+                        Modifier.align(Alignment.TopEnd)
+                            .padding(8.dp)
+                            .testTag("close_button_hamburger_menu")
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Close,
+                            contentDescription = "Close button hamburger menu"
+                        )
+                    }
+                }
+
+                // Display the navigation items
+                items.forEachIndexed { index, item ->
+                    NavigationDrawerItem(
+                        label = { Text(text = item.title) },
+                        selected = index == selectedItemIndex,
+                        onClick = {
+                            selectedItemIndex = index
+                            scope.launch { drawerState.close() }
+                            item.navOnClick?.invoke()
+                        },
+                        icon = {
+                            Icon(
+                                imageVector =
+                                if (index == selectedItemIndex) {
+                                    item.selectedIcon
+                                } else {
+                                    item.unselectedIcon
+                                },
+                                contentDescription = item.title
+                            )
+                        },
+                        badge = {
+                            item.badgeCount?.let { Text(text = item.badgeCount.toString()) }
+                        },
+                        modifier =
+                        Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                            .testTag("navigation_item_$index")
+                    )
+                }
+            }
         },
         floatingActionButton = { SearchButton(onClick = { overlay.value = Overlay.SEARCH_SHEET }) }
     ) { paddingValues ->
