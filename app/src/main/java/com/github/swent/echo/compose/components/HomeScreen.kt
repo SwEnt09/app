9f6b12a0fb027acbc56ab4c99c8e792f6c11b63f
Noah Robert <noah.robert@epfl.ch>
2024-04-10 14:51:36 +0200

    created the Scaffold for the HomeScreen, still needs some other things to work 100% (repository, events on map, etc.)

--- app/src/main/java/com/github/swent/echo/compose/components/HomeScreen.kt
+++ app/src/main/java/com/github/swent/echo/compose/components/HomeScreen.kt
@@ -11,13 +11,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.github.swent.echo.compose.map.MapDrawer
 import com.github.swent.echo.data.model.Event
+import com.github.swent.echo.data.model.Location
 import com.github.swent.echo.ui.navigation.NavigationActions
+import java.util.Date
 
 enum class Overlay {
     NONE,
     EVENT_INFO_SHEET,
-    SEARCH_SHEET,
-    HAMBURGER_MENU
+    SEARCH_SHEET
 }
 
 enum class MapOrListMode {
@@ -25,26 +26,19 @@ enum class MapOrListMode {
     LIST
 }
 
-data class EventInfo(
-    val image: Int,
-    val people: Int,
-    val maxPeople: Int,
-    val hostName: String,
-    val event: Event
-)
-
 /**
  * The scaffold for the home screen. contains the top bar, the bottom sheet if an event is selected,
- * the hamburger menu when the button is clicked on the top bar, the search bottom sheet, the list view, and the map.
+ * the hamburger menu when the button is clicked on the top bar, the search bottom sheet, the list
+ * view, and the map.
  */
 @Composable
-fun HomeScreen(navActions: NavigationActions) {
+fun HomeScreen(navActions: NavigationActions? = null) {
     val overlay = remember { mutableStateOf(Overlay.NONE) }
     val mode = remember { mutableStateOf(MapOrListMode.MAP) }
     Scaffold(
         topBar = {
             TopBar(
-                { overlay.value = Overlay.HAMBURGER_MENU },
+                {},
                 {
                     if (mode.value == MapOrListMode.MAP) {
                         mode.value = MapOrListMode.LIST
@@ -65,7 +59,7 @@ private fun Content(
     paddingValues: PaddingValues,
     overlay: MutableState<Overlay>,
     mode: MutableState<MapOrListMode>,
-    navActions: NavigationActions
+    navActions: NavigationActions?
 ) {
     Box(modifier = Modifier.padding(paddingValues)) {
         if (mode.value == MapOrListMode.LIST) {
@@ -75,28 +69,37 @@ private fun Content(
         }
         // add the tag filtering here
         if (overlay.value == Overlay.EVENT_INFO_SHEET) {
-            // TODO get the event information from the repository
-            /*
+            // TODO get the event from the repository
+            val event =
+                Event(
+                    eventId = "1",
+                    organizerId = "1",
+                    title = "Event Title",
+                    description = "Event Description",
+                    location = Location("", 0.0, 0.0),
+                    startDate = Date(2024, 5, 18, 18, 15),
+                    endDate = Date(2024, 1, 1, 22, 0),
+                    tags = setOf()
+                )
             EventInfoSheet(
-                eventImage = image,
-                eventPeople = people,
-                eventPeopleMax = maxPeople,
-                hostName = hostName,
-                event = event,
+                eventImage = 0 /* TODO get the image from the repository*/,
+                eventPeople = 2 /* TODO get the number of people from the repository */,
+                eventPeopleMax = 4 /* TODO get the max number of people from the repository */,
+                hostName = "Bowling club" /* TODO get the host name from the repository */,
+                event = event /* TODO get the event from the repository */,
                 onJoinButtonPressed = {},
                 onShowPeopleButtonPressed = {},
                 onDismiss = { overlay.value = Overlay.NONE },
                 onFullyExtended = {}
-            ) */
-              // {navActions.navigateTo(Routes.EventInfoScreen)}) <- when we make a whole screen for
-              // the event info
+            )
+            // {navActions.navigateTo(Routes.EventInfoScreen)}) <- when we make a whole screen for
+            // the event info
         }
 
         if (overlay.value == Overlay.SEARCH_SHEET) {
-            // TODO add the search bottom sheet
-        }
-        if (overlay.value == Overlay.HAMBURGER_MENU) {
-            // TODO add the hamburger menu
+            SearchMenuSheet(onFullyExtended = {}, onDismiss = { overlay.value = Overlay.NONE })
+            // {navActions.navigateTo(Routes.SearchScreen)}) <- when we make a whole screen for
+            // the search menu
         }
     }
 }
