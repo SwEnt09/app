eee199bfb472b75c6b1f4dfe5a097b424160a268
yoan <yoan@laptop>
2024-04-11 12:14:42 +0200

    add more tests: fail cases

--- app/src/test/java/com/github/swent/echo/viewmodels/event/EventViewModelTest.kt
+++ app/src/test/java/com/github/swent/echo/viewmodels/event/EventViewModelTest.kt
@@ -1,11 +1,17 @@
 package com.github.swent.echo.viewmodels.event
 
+import android.util.Log
 import com.github.swent.echo.data.model.Event
 import com.github.swent.echo.data.model.Location
 import com.github.swent.echo.data.model.Tag
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.verify
 import java.time.Instant
 import java.util.Date
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNull
+import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
 class EventViewModelTest {
@@ -21,6 +27,15 @@ class EventViewModelTest {
             endDate = Date.from(Instant.now()),
             tags = setOf(Tag("1", "tag1"))
         )
+
+    private fun mockLog() {
+        mockkStatic(Log::class)
+        every { Log.v(any(), any()) } returns 0
+        every { Log.d(any(), any()) } returns 0
+        every { Log.i(any(), any()) } returns 0
+        every { Log.w(any(), any() as String) } returns 0
+        every { Log.e(any(), any()) } returns 0
+    }
 
     @Test
     fun modifyEventTest() {
@@ -47,5 +62,57 @@ class EventViewModelTest {
         eventViewModel.deleteTag(Tag("1", "tag1"))
         assertEquals(eventViewModel.getEvent().tags, setOf<Tag>())
     }
-    
+
+    @Test
+    fun modifyEventWhileSavingLogWarningAndDoesNotChangeEvent() {
+        mockLog()
+        val event = TEST_EVENT
+        val eventModified = event.copy(title = "change title")
+        val eventViewModel = EventViewModel()
+        eventViewModel.setEvent(event)
+        eventViewModel.saveEvent()
+        eventViewModel.setEvent(eventModified)
+        verify { Log.w(any(), any() as String) }
+        assertEquals(event, eventViewModel.getEvent())
+    }
+
+    @Test
+    fun saveEventWhileSavingLogWarning() {
+        mockLog()
+        val eventViewModel = EventViewModel()
+        eventViewModel.setEvent(TEST_EVENT)
+        eventViewModel.saveEvent()
+        eventViewModel.saveEvent()
+        verify { Log.w(any(), any() as String) }
+    }
+
+    @Test
+    fun getAndAddTagFromStringReturnNullWithWrongTag() {
+        val wrongTag = "not a valid tag"
+        val eventViewModel = EventViewModel()
+        val ret = eventViewModel.getAndAddTagFromString(wrongTag)
+        assertNull(ret)
+    }
+
+    @Test
+    fun saveWithEndDateBeforeStartDateChangeStatusToError() {
+        val event =
+            TEST_EVENT.copy(
+                startDate = Date.from(Instant.now()),
+                endDate = Date.from(Instant.EPOCH)
+            )
+        val eventViewModel = EventViewModel()
+        eventViewModel.setEvent(event)
+        eventViewModel.saveEvent()
+        assertTrue(eventViewModel.getStatus() is EventStatus.Error)
+    }
+
+    @Test
+    fun saveWithBlankTitleChangeStatusToError() {
+        val event = TEST_EVENT.copy(title = " ")
+        val eventViewModel = EventViewModel()
+        eventViewModel.setEvent(event)
+        eventViewModel.saveEvent()
+        assertTrue(eventViewModel.getStatus() is EventStatus.Error)
+    }
 }
