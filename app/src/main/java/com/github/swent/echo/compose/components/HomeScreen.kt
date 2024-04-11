2e080edf062193b1b02437d8da7ee5621601bbd0
Noah Robert <noah.robert@epfl.ch>
2024-04-11 13:35:28 +0200

    added a method to call for when the map is updated with a callback, New dataclass to go with this as well
    commented the methods and dataclasses used by the map composable, because it's not available yet

--- app/src/main/java/com/github/swent/echo/compose/components/HomeScreen.kt
+++ app/src/main/java/com/github/swent/echo/compose/components/HomeScreen.kt
@@ -10,10 +10,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import com.github.swent.echo.compose.map.MAP_CENTER
 import com.github.swent.echo.compose.map.MapDrawer
 import com.github.swent.echo.data.model.Event
 import com.github.swent.echo.data.model.Location
 import com.github.swent.echo.ui.navigation.NavigationActions
+import java.time.Instant
 import java.util.Date
 
 enum class Overlay {
@@ -27,6 +29,14 @@ enum class MapOrListMode {
     LIST
 }
 
+data class DisplayEventInfo(
+    val event: Event,
+    val eventImage: Int,
+    val eventPeople: Int,
+    val eventPeopleMax: Int,
+    val hostName: String
+)
+
 /**
  * The scaffold for the home screen. contains the top bar, the bottom sheet if an event is selected,
  * the hamburger menu when the button is clicked on the top bar, the search bottom sheet, the list
@@ -36,6 +46,7 @@ enum class MapOrListMode {
 fun HomeScreen(navActions: NavigationActions) {
     val overlay = remember { mutableStateOf(Overlay.NONE) }
     val mode = remember { mutableStateOf(MapOrListMode.MAP) }
+    val displayEventInfo = remember { mutableStateOf<DisplayEventInfo?>(null) }
     Scaffold(
         modifier = Modifier.testTag("home_screen"),
         topBar = {
@@ -52,7 +63,7 @@ fun HomeScreen(navActions: NavigationActions) {
         },
         floatingActionButton = { SearchButton(onClick = { overlay.value = Overlay.SEARCH_SHEET }) }
     ) { paddingValues ->
-        Content(paddingValues, overlay, mode, navActions)
+        Content(paddingValues, overlay, mode, navActions, displayEventInfo)
     }
 }
 
@@ -61,39 +72,68 @@ private fun Content(
     paddingValues: PaddingValues,
     overlay: MutableState<Overlay>,
     mode: MutableState<MapOrListMode>,
-    navActions: NavigationActions
+    navActions: NavigationActions,
+    displayEventInfo: MutableState<DisplayEventInfo?>
 ) {
+
+    fun onEventSelected(
+        event: Event,
+        eventImage: Int = 0,
+        eventPeople: Int = 0,
+        eventPeopleMax: Int = 0,
+        hostName: String = ""
+    ) {
+        displayEventInfo.value =
+            DisplayEventInfo(event, eventImage, eventPeople, eventPeopleMax, hostName)
+        overlay.value = Overlay.EVENT_INFO_SHEET
+    }
+
+    val events =
+        listOf(
+            Event(
+                eventId = "a",
+                organizerId = "a",
+                title = "Bowling Event",
+                description = "",
+                location = Location("Location 1", MAP_CENTER.toGeoPoint()),
+                startDate = Date.from(Instant.now()),
+                endDate = Date.from(Instant.now()),
+                tags = emptySet(),
+            ),
+            Event(
+                eventId = "b",
+                organizerId = "a",
+                title = "Swimming Event",
+                description = "",
+                location =
+                    Location("Location 2", MAP_CENTER.toGeoPoint().destinationPoint(1000.0, 90.0)),
+                startDate = Date.from(Instant.now()),
+                endDate = Date.from(Instant.now()),
+                tags = emptySet(),
+            )
+        )
+
     Box(modifier = Modifier.padding(paddingValues)) {
         if (mode.value == MapOrListMode.LIST) {
             // TODO add the list view
         } else {
-            MapDrawer()
+            MapDrawer(events = events, callback = { event -> onEventSelected(event) })
         }
         // add the tag filtering here
-        if (overlay.value == Overlay.EVENT_INFO_SHEET) {
-            // TODO get the event from the repository
-            val event =
-                Event(
-                    eventId = "1",
-                    organizerId = "1",
-                    title = "Event Title",
-                    description = "Event Description",
-                    location = Location("", 0.0, 0.0),
-                    startDate = Date(2024, 5, 18, 18, 15),
-                    endDate = Date(2024, 1, 1, 22, 0),
-                    tags = setOf()
-                )
+        if (overlay.value == Overlay.EVENT_INFO_SHEET && displayEventInfo.value != null) {
+
             EventInfoSheet(
-                eventImage = 0 /* TODO get the image from the repository*/,
-                eventPeople = 2 /* TODO get the number of people from the repository */,
-                eventPeopleMax = 4 /* TODO get the max number of people from the repository */,
-                hostName = "Bowling club" /* TODO get the host name from the repository */,
-                event = event /* TODO get the event from the repository */,
+                eventImage = displayEventInfo.value!!.eventImage,
+                eventPeople = displayEventInfo.value!!.eventPeople,
+                eventPeopleMax = displayEventInfo.value!!.eventPeopleMax,
+                hostName = displayEventInfo.value!!.hostName,
+                event = displayEventInfo.value!!.event,
                 onJoinButtonPressed = {},
                 onShowPeopleButtonPressed = {},
                 onDismiss = { overlay.value = Overlay.NONE },
                 onFullyExtended = {}
             )
+
             // {navActions.navigateTo(Routes.EventInfoScreen)}) <- when we make a whole screen for
             // the event info
         }
