38d9490d0861bdf32e47d631cf761e3e6cb3d7f9
Nathan Tissières <nathan.tissieres@epfl.ch>
2024-04-11 13:42:27 +0200

    Adding tests for hamburger menu and adapting the old ones

--- app/src/androidTest/java/com/github/swent/echo/compose/components/TopBarTest.kt
+++ app/src/androidTest/java/com/github/swent/echo/compose/components/TopBarTest.kt
@@ -4,6 +4,8 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import com.github.swent.echo.ui.navigation.NavigationActions
+import io.mockk.mockk
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
@@ -14,14 +16,14 @@ class TopBarTest {
 
     @get:Rule val composeTestRule = createComposeRule()
 
-    private var menuClicked = 0
     private var searchClicked = 0
+    private lateinit var navActions: NavigationActions
 
     @Before
     fun setUp() {
-        menuClicked = 0
         searchClicked = 0
-        composeTestRule.setContent { TopBar({ menuClicked++ }, { searchClicked++ }) }
+        navActions = mockk(relaxed = true)
+        composeTestRule.setContent { TopBar(navActions, { searchClicked++ }) }
     }
 
     @Test
@@ -30,14 +32,64 @@ class TopBarTest {
     }
 
     @Test
-    fun shouldCallOpenMenuCallbackWhenMenuButtonClicked() {
-        composeTestRule.onNodeWithTag("menu_button").performClick()
-        assertThat(menuClicked, equalTo(1))
+    fun shouldCallOpenSearchCallbackWhenSearchButtonClicked() {
+        composeTestRule.onNodeWithTag("list_map_mode_button").performClick()
+        assertThat(searchClicked, equalTo(1))
     }
 
     @Test
-    fun shouldCallOpenSearchCallbackWhenSearchButtonClicked() {
-        composeTestRule.onNodeWithTag("search_button").performClick()
-        assertThat(searchClicked, equalTo(1))
+    fun shouldOpenHamburgerMenuWhenMenuButtonClicked() {
+        composeTestRule.onNodeWithTag("menu_button").performClick()
+        composeTestRule.onNodeWithTag("hamburger_menu").assertExists()
+    }
+
+    @Test
+    fun shouldShowProfileBoxWhenMenuButtonClicked() {
+        composeTestRule.onNodeWithTag("menu_button").performClick()
+        composeTestRule.onNodeWithTag("profile_box").assertExists()
+    }
+
+    @Test
+    fun shouldShowProfileSheetWhenMenuButtonClicked() {
+        composeTestRule.onNodeWithTag("menu_button").performClick()
+        composeTestRule.onNodeWithTag("profile_sheet").assertExists()
+    }
+
+    @Test
+    fun shouldShowProfilePictureWhenMenuButtonClicked() {
+        composeTestRule.onNodeWithTag("menu_button").performClick()
+        composeTestRule.onNodeWithTag("profile_picture").assertExists()
+    }
+
+    @Test
+    fun shouldShowProfileInfoWhenMenuButtonClicked() {
+        composeTestRule.onNodeWithTag("menu_button").performClick()
+        composeTestRule.onNodeWithTag("profile_info").assertExists()
+    }
+
+    @Test
+    fun shouldShowProfileNameWhenMenuButtonClicked() {
+        composeTestRule.onNodeWithTag("menu_button").performClick()
+        composeTestRule.onNodeWithTag("profile_name").assertExists()
+    }
+
+    @Test
+    fun shouldShowProfileClassWhenMenuButtonClicked() {
+        composeTestRule.onNodeWithTag("menu_button").performClick()
+        composeTestRule.onNodeWithTag("profile_class").assertExists()
+    }
+
+    @Test
+    fun shouldShowDarkModeButtonWhenMenuButtonClicked() {
+        composeTestRule.onNodeWithTag("menu_button").performClick()
+        composeTestRule.onNodeWithTag("dark_mode_button").assertExists()
+    }
+
+    @Test
+    fun shouldShowAllItemsWhenMenuButtonClicked() {
+        composeTestRule.onNodeWithTag("menu_button").performClick()
+        for (i in 0..6) {
+            composeTestRule.onNodeWithTag("navigation_item_$i").assertExists()
+        }
     }
 }
