76a073620934c68c488f0ba4d3f23ec1f4133503
yoan <yoan@laptop>
2024-04-11 12:09:37 +0200

    fix wrong return value

--- app/src/main/java/com/github/swent/echo/viewmodels/event/EventViewModel.kt
+++ app/src/main/java/com/github/swent/echo/viewmodels/event/EventViewModel.kt
@@ -88,7 +88,7 @@ class EventViewModel() : ViewModel() {
         } else if (_status.value == EventStatus.Saved) {
             Log.w("save event", "trying to save the event but it's already saved")
         } else {
-            if(eventIsValid()){
+            if (eventIsValid()) {
                 _status.value = EventStatus.Saving
                 // TODO: save in repository
             }
@@ -100,19 +100,16 @@ class EventViewModel() : ViewModel() {
         return _status.asStateFlow().value
     }
 
-    /**
-     * check the current event has valid data
-     * if not return false and set _status to Error
-     */
+    /** check the current event has valid data if not return false and set _status to Error */
     private fun eventIsValid(): Boolean {
-        val event  = _event.value
-        if(event.startDate.after(event.endDate)){
+        val event = _event.value
+        if (event.startDate.after(event.endDate)) {
             _status.value = EventStatus.Error("end date before start date")
         }
-        if(event.title.isBlank()){
+        if (event.title.isBlank()) {
             _status.value = EventStatus.Error("title is empty")
         }
-        return _status.value is EventStatus.Error
+        return _status.value !is EventStatus.Error
     }
 }
 
