15244b9597f0bd8b553051eb9507cca98241e7d2
alejandrocalles <gabriel.jimenezcalles@epfl.ch>
2024-04-11 11:55:30 +0200

    Enhance the `Location` data class.
    Add the `toLatLng` method to get the `LatLng` representation of a
    `Location`.

--- app/src/main/java/com/github/swent/echo/data/model/Location.kt
+++ app/src/main/java/com/github/swent/echo/data/model/Location.kt
@@ -1,13 +1,22 @@
 package com.github.swent.echo.data.model
 
+import org.maplibre.android.geometry.LatLng
 import org.osmdroid.util.GeoPoint
 
 data class Location(val name: String, val lat: Double, val long: Double) {
     constructor(name: String, point: GeoPoint) : this(name, point.latitude, point.longitude)
+    constructor(name: String, point: LatLng) : this(name, point.latitude, point.longitude)
     /**
      * Transform this location into a [GeoPoint].
      *
-     * @return The respective [GeoPoint]
+     * @return The respective [GeoPoint].
      */
     fun toGeoPoint() = GeoPoint(lat, long)
+
+    /**
+     * Transform this location into a [LatLng].
+     *
+     * @return The respective [LatLng].
+     */
+    fun toLatLng() = LatLng(lat, long)
 }
