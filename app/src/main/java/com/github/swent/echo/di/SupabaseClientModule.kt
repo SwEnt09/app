318533e78482f8658e8dcd5e31cc528c2a04559c
octogradiste <colin.berger@outlook.com>
2024-04-11 23:18:43 +0200

    Installing the compose auth supabase plugin

--- app/src/main/java/com/github/swent/echo/di/SupabaseClientModule.kt
+++ app/src/main/java/com/github/swent/echo/di/SupabaseClientModule.kt
@@ -7,6 +7,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.compose.auth.ComposeAuth
+import io.github.jan.supabase.compose.auth.googleNativeLogin
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.gotrue.Auth
 import io.github.jan.supabase.postgrest.Postgrest
@@ -19,9 +21,11 @@ object SupabaseClientModule {
     fun provideSupabaseClient(application: Application): SupabaseClient {
         val supabaseUrl = application.resources.getString(R.string.supabase_url)
         val supabasePublicKey = application.resources.getString(R.string.supabase_public_key)
+        val googleWebClientId = application.resources.getString(R.string.google_web_client_id)
         return createSupabaseClient(supabaseUrl, supabasePublicKey) {
             install(Auth)
             install(Postgrest)
+            install(ComposeAuth) { googleNativeLogin(googleWebClientId) }
         }
     }
 }
