99aac18bb38a39a4d87d837ab39953ffc626d9d9
Noah Robert <noah.robert@epfl.ch>
2024-04-12 11:44:18 +0200

    new tests and line optimisation
    added lines and tests

--- app/src/androidTest/java/com/github/swent/echo/compose/components/HomeScreenTest.kt
+++ app/src/androidTest/java/com/github/swent/echo/compose/components/HomeScreenTest.kt
@@ -3,8 +3,13 @@ package com.github.swent.echo.compose.components
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import com.github.swent.echo.compose.map.MAP_CENTER
+import com.github.swent.echo.data.model.Event
+import com.github.swent.echo.data.model.Location
 import com.github.swent.echo.ui.navigation.NavigationActions
 import io.mockk.mockk
+import java.time.Instant
+import java.util.Date
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -51,5 +56,26 @@ class HomeScreenTest {
     fun shouldShowSearchMenuSheet() {
         composeTestRule.onNodeWithTag("search_button").performClick()
         composeTestRule.onNodeWithTag("search_menu_sheet").assertExists()
+    }
+
+    @Test
+    fun shouldNotShowEventInfoSheet() {
+        composeTestRule.onNodeWithTag("event_info_sheet").assertDoesNotExist()
+    }
+
+    @Test
+    fun shouldShowEventInfoDataClass() {
+        val event =
+            Event(
+                eventId = "a",
+                organizerId = "a",
+                title = "Bowling Event",
+                description = "",
+                location = Location("Location 1", MAP_CENTER.toGeoPoint()),
+                startDate = Date.from(Instant.now()),
+                endDate = Date.from(Instant.now()),
+                tags = emptySet(),
+            )
+        val obj = DisplayEventInfo(event, 0, 0, 0, "")
     }
 }
