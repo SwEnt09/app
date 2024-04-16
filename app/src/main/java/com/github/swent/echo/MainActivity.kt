60a64c77db1131826b8f6461012fd5902d3ca9d0
octogradiste <colin.berger@outlook.com>
2024-04-16 21:44:44 +0200

    Skip log in process if the user was already logged in before

--- app/src/main/java/com/github/swent/echo/MainActivity.kt
+++ app/src/main/java/com/github/swent/echo/MainActivity.kt
@@ -7,14 +7,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.github.swent.echo.authentication.AuthenticationService
 import com.github.swent.echo.compose.navigation.AppNavigationHost
 import com.github.swent.echo.ui.theme.EchoTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import kotlinx.coroutines.runBlocking
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject lateinit var authenticationService: AuthenticationService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // This happens so quickly, I doesn't make sense to show a loading screen.
+        runBlocking { authenticationService.initialize() }
+
         setContent {
             EchoTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AppNavigationHost()
+                    AppNavigationHost(userIsLoggedIn = authenticationService.userIsLoggedIn())
                 }
             }
         }
