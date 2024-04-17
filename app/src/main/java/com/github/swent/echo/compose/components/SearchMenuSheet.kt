6229baaf02e107816828fbb9924f3cf3169e497d
Nathan Tissières <nathan.tissieres@epfl.ch>
2024-04-17 20:57:25 +0200

    Main look of the SearchMenuSheet, added the search bar, switch mode button (between dicover and filter) and close search button

--- app/src/main/java/com/github/swent/echo/compose/components/SearchMenuSheet.kt
+++ app/src/main/java/com/github/swent/echo/compose/components/SearchMenuSheet.kt
@@ -1,10 +1,8 @@
 package com.github.swent.echo.compose.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,13 +11,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -35,16 +29,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.github.swent.echo.compose.components.searchmenu.SearchMenuDiscover
+import com.github.swent.echo.compose.components.searchmenu.SearchMenuFilters
 
-enum class SearchMode(val switchToName: String, val switchToIcon: ImageVector){
+enum class SearchMode(val switchToName: String, val switchToIcon: ImageVector) {
     FILTERS("Discover", Icons.Filled.ShoppingCart),
     DISCOVER("Filters", Icons.Filled.Settings)
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchMenuSheet(onFullyExtended: () -> Unit, onDismiss: () -> Unit) {
@@ -52,7 +47,7 @@ fun SearchMenuSheet(onFullyExtended: () -> Unit, onDismiss: () -> Unit) {
     var searched by remember { mutableStateOf("") }
 
     // Search mode
-    var searchMode by remember { mutableStateOf(SearchMode.DISCOVER) }
+    var searchMode by remember { mutableStateOf(SearchMode.FILTERS) }
 
     // Sheet content
     val sheetState =
@@ -63,19 +58,12 @@ fun SearchMenuSheet(onFullyExtended: () -> Unit, onDismiss: () -> Unit) {
             true
         }
     ModalBottomSheet(
-        modifier = Modifier
-            .fillMaxSize()
-            .testTag("search_menu_sheet"),
+        modifier = Modifier.fillMaxSize().testTag("search_menu_sheet"),
         onDismissRequest = onDismiss,
         sheetState = sheetState,
         containerColor = MaterialTheme.colorScheme.primaryContainer
     ) {
-        Box (
-            modifier = Modifier
-                .background(Color.White)
-                .fillMaxSize()
-                .padding(5.dp)
-        ){
+        Box(modifier = Modifier.fillMaxSize().padding(5.dp)) {
             // Sheet content
             Row(
                 verticalAlignment = Alignment.CenterVertically,
@@ -86,43 +74,45 @@ fun SearchMenuSheet(onFullyExtended: () -> Unit, onDismiss: () -> Unit) {
                     label = { Text("Search hobby/categorie...") },
                     value = searched,
                     onValueChange = { searched = it },
-                    modifier = Modifier
-                        .padding(5.dp)
-                        .width(240.dp)
+                    modifier = Modifier.width(240.dp)
                 )
+                Spacer(modifier = Modifier.width(10.dp))
                 // Switch search mode button
                 Button(
                     onClick = {
-                        searchMode = if (searchMode == SearchMode.FILTERS) {
-                            SearchMode.DISCOVER
-                        } else {
-                            SearchMode.FILTERS
-                        }
+                        searchMode =
+                            if (searchMode == SearchMode.FILTERS) {
+                                SearchMode.DISCOVER
+                            } else {
+                                SearchMode.FILTERS
+                            }
                     },
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier.fillMaxWidth().height(40.dp)
                 ) {
                     Icon(searchMode.switchToIcon, contentDescription = searchMode.switchToName)
                     Text(searchMode.switchToName)
                 }
             }
             // Display filters or discover according to the selected mode
-            if (searchMode == SearchMode.FILTERS) {
-                SearchMenuFilters()
-            } else {
-                SearchMenuDiscover()
+            Box(
+                modifier =
+                    Modifier.align(Alignment.TopCenter).fillMaxWidth().absoluteOffset(y = 70.dp)
+            ) {
+                if (searchMode == SearchMode.FILTERS) {
+                    SearchMenuFilters()
+                } else {
+                    SearchMenuDiscover()
+                }
             }
             // Close Search Button
             Box(
-                modifier = Modifier.align(Alignment.Center).fillMaxWidth()
+                modifier =
+                    Modifier.align(Alignment.TopCenter).fillMaxWidth().absoluteOffset(y = 300.dp)
             ) {
-                Button(
-                    onClick = { /*TODO*/ },
-                    modifier = Modifier.align(Alignment.Center).absoluteOffset(y = (-50).dp)
-                ) {
+                Button(onClick = { /*TODO*/}, modifier = Modifier.align(Alignment.Center)) {
                     Icon(Icons.Filled.Close, contentDescription = "Close Search")
                 }
             }
         }
     }
 }
-
