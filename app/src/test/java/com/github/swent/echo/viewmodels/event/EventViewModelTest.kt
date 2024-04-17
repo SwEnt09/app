e1dda1d7b74b23be3d4df2755fdffe230d3f7468
yoan <yoan@laptop>
2024-04-17 13:52:26 +0200

    adapt current tests & add tests for repository calls

--- app/src/test/java/com/github/swent/echo/viewmodels/event/EventViewModelTest.kt
+++ app/src/test/java/com/github/swent/echo/viewmodels/event/EventViewModelTest.kt
@@ -6,13 +6,29 @@ import com.github.swent.echo.data.model.Event
 import com.github.swent.echo.data.model.Location
 import com.github.swent.echo.data.model.Tag
 import com.github.swent.echo.data.model.UserProfile
+import com.github.swent.echo.data.model.UserProfile
+import com.github.swent.echo.data.repository.Repository
+import com.github.swent.echo.fakes.FakeAuthenticationService
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
+import io.mockk.mockk
 import io.mockk.mockkStatic
 import io.mockk.verify
+import java.time.Instant
+import java.util.Date
+import java.util.concurrent.CompletableFuture
 import java.time.ZonedDateTime
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNull
 import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.future.await
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineScheduler
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
 import org.junit.Test
 
 class EventViewModelTest {
@@ -42,49 +58,73 @@ class EventViewModelTest {
         every { Log.e(any(), any()) } returns 0
     }
 
+    fun blockOnSaving() {
+        coEvery { mockedRepository.setEvent(any()) } coAnswers
+            {
+                CompletableFuture<Event>().await()
+            }
+    }
+
+    private var associationList = listOf(Association("0", "a0", "desc0"))
+    private var eventList = listOf(TEST_EVENT)
+    private val fakeAuthenticationService = FakeAuthenticationService()
+
+    private val mockedRepository = mockk<Repository>(relaxed = true)
+    private lateinit var eventViewModel: EventViewModel
+    private val scheduler = TestCoroutineScheduler()
+
+    @Before
+    fun init() {
+        fakeAuthenticationService.userID = "u0"
+        // val scheduler = TestCoroutineScheduler()
+        Dispatchers.setMain(StandardTestDispatcher(scheduler))
+        runBlocking { eventViewModel = EventViewModel(mockedRepository, fakeAuthenticationService) }
+        scheduler.runCurrent()
+    }
+
     @Test
     fun modifyEventTest() {
         val event = TEST_EVENT
-        val eventViewModel = EventViewModel()
         eventViewModel.setEvent(event)
-        assertEquals(eventViewModel.getEvent(), event)
+        assertEquals(eventViewModel.getEvent().value, event)
     }
 
     @Test
     fun addTagToEventTest() {
-        val eventViewModel = EventViewModel()
         val newTag = Tag("2", "tag2")
+        coEvery { mockedRepository.getAllTags() } returns listOf(newTag)
+        runBlocking { eventViewModel = EventViewModel(mockedRepository, fakeAuthenticationService) }
+        scheduler.runCurrent()
         val addedTag = eventViewModel.getAndAddTagFromString(newTag.name)
         assertEquals(addedTag, newTag)
-        assertEquals(eventViewModel.getEvent().tags, setOf(newTag))
+        assertEquals(eventViewModel.getEvent().value.tags, setOf(newTag))
     }
 
     @Test
     fun deleteTagFromEventTest() {
         val event = TEST_EVENT
-        val eventViewModel = EventViewModel()
         eventViewModel.setEvent(event)
         eventViewModel.deleteTag(Tag("1", "tag1"))
-        assertEquals(eventViewModel.getEvent().tags, setOf<Tag>())
+        assertEquals(eventViewModel.getEvent().value.tags, setOf<Tag>())
     }
 
     @Test
     fun modifyEventWhileSavingLogWarningAndDoesNotChangeEvent() {
         mockLog()
+        blockOnSaving()
         val event = TEST_EVENT
         val eventModified = event.copy(title = "change title")
-        val eventViewModel = EventViewModel()
         eventViewModel.setEvent(event)
         eventViewModel.saveEvent()
         eventViewModel.setEvent(eventModified)
         verify { Log.w(any(), any() as String) }
-        assertEquals(event, eventViewModel.getEvent())
+        assertEquals(event, eventViewModel.getEvent().value)
     }
 
     @Test
     fun saveEventWhileSavingLogWarning() {
         mockLog()
-        val eventViewModel = EventViewModel()
+        blockOnSaving()
         eventViewModel.setEvent(TEST_EVENT)
         eventViewModel.saveEvent()
         eventViewModel.saveEvent()
@@ -94,7 +134,6 @@ class EventViewModelTest {
     @Test
     fun getAndAddTagFromStringReturnNullWithWrongTag() {
         val wrongTag = "not a valid tag"
-        val eventViewModel = EventViewModel()
         val ret = eventViewModel.getAndAddTagFromString(wrongTag)
         assertNull(ret)
     }
@@ -106,18 +145,111 @@ class EventViewModelTest {
                 startDate = ZonedDateTime.now(),
                 endDate = ZonedDateTime.now().minusDays(1)
             )
-        val eventViewModel = EventViewModel()
         eventViewModel.setEvent(event)
         eventViewModel.saveEvent()
-        assertTrue(eventViewModel.getStatus() is EventStatus.Error)
+        assertTrue(eventViewModel.getStatus().value is EventStatus.Error)
     }
 
     @Test
     fun saveWithBlankTitleChangeStatusToError() {
         val event = TEST_EVENT.copy(title = " ")
-        val eventViewModel = EventViewModel()
         eventViewModel.setEvent(event)
         eventViewModel.saveEvent()
-        assertTrue(eventViewModel.getStatus() is EventStatus.Error)
+        assertTrue(eventViewModel.getStatus().value is EventStatus.Error)
+    }
+
+    @Test
+    fun getOrganizerNameThrowErrorWhenOrganizerIdIsNotInRepository() {
+        mockLog()
+        coEvery { mockedRepository.getUserProfile(any()) } returns UserProfile("", "")
+        coEvery { mockedRepository.getAssociation(any()) } returns Association("", "", "")
+        val organizerName = eventViewModel.getOrganizerName()
+        scheduler.runCurrent()
+        coVerify { mockedRepository.getUserProfile(any()) }
+        coVerify { mockedRepository.getAssociation(any()) }
+        verify { Log.e(any(), any()) }
+        assertEquals("", organizerName.value)
+    }
+
+    @Test
+    fun getOrganizerNameReturnsCorrectName() {
+        val testUserProfile = UserProfile("testid", "testname")
+        coEvery { mockedRepository.getUserProfile(any()) } returns testUserProfile
+        val organizerName = eventViewModel.getOrganizerName()
+        scheduler.runCurrent()
+        coVerify { mockedRepository.getUserProfile(any()) }
+        assertEquals(testUserProfile.name, organizerName.value)
+    }
+
+    @Test
+    fun getOrganizerListThrowErrorWhenThereIsNoOrganizerAvailable() {
+        mockLog()
+        coEvery { mockedRepository.getUserProfile(any()) } returns UserProfile("", "")
+        val organizerList = eventViewModel.getOrganizerList()
+        scheduler.runCurrent()
+        coVerify { mockedRepository.getUserProfile(any()) }
+        verify { Log.e(any(), any()) }
+        assertEquals(listOf<String>(), organizerList.value)
+    }
+
+    @Test
+    fun getOrganizerListReturnsCorrectUsername() {
+        val testUserProfile = UserProfile("testid", "testname")
+        coEvery { mockedRepository.getUserProfile(any()) } returns testUserProfile
+        val organizerList = eventViewModel.getOrganizerList()
+        scheduler.runCurrent()
+        coVerify { mockedRepository.getUserProfile(any()) }
+        assertEquals(listOf(testUserProfile.name), organizerList.value)
+    }
+
+    @Test
+    fun setOrganizerNameAsEventCreatorSetTheCorrectValue() {
+        val testUserProfile = UserProfile(TEST_EVENT.creatorId, "testname")
+        eventViewModel.setEvent(TEST_EVENT)
+        coEvery { mockedRepository.getUserProfile(any()) } returns testUserProfile
+        eventViewModel.setOrganizer(testUserProfile.name)
+        scheduler.runCurrent()
+        assertEquals(TEST_EVENT.organizerId, eventViewModel.getEvent().value.organizerId)
+    }
+
+    @Test
+    fun setOrganizerNameAsOtherUserThrowError() {
+        mockLog()
+        val testUserProfile = UserProfile(TEST_EVENT.creatorId, "testname")
+        eventViewModel.setEvent(TEST_EVENT)
+        coEvery { mockedRepository.getUserProfile(any()) } returns testUserProfile
+        eventViewModel.setOrganizer("anothername")
+        scheduler.runCurrent()
+        verify { Log.e(any(), any()) }
+    }
+
+    @Test
+    fun setOrganizerNameAsAssociationSetTheCorrectValue() {
+        val testAssociation = Association("testAid", "testAname", "testDescription")
+        eventViewModel.setEvent(TEST_EVENT)
+        coEvery { mockedRepository.getAllAssociations() } returns listOf(testAssociation)
+        eventViewModel.setOrganizer(testAssociation.name)
+        scheduler.runCurrent()
+        assertEquals(testAssociation.associationId, eventViewModel.getEvent().value.organizerId)
+    }
+
+    @Test
+    fun modifyExistingEventUpdateItInTheRepository() {
+        val existingEvent = TEST_EVENT
+        coEvery { mockedRepository.getEvent(any()) } returns existingEvent
+        runBlocking {
+            eventViewModel =
+                EventViewModel(mockedRepository, fakeAuthenticationService, existingEvent.eventId)
+        }
+        scheduler.runCurrent()
+        assertEquals(EventStatus.Saved, eventViewModel.getStatus().value)
+        val modifiedExistingEvent = existingEvent.copy(title = "another title")
+        eventViewModel.setEvent(modifiedExistingEvent)
+        assertEquals(EventStatus.Modified, eventViewModel.getStatus().value)
+        eventViewModel.saveEvent()
+        assertEquals(EventStatus.Saving, eventViewModel.getStatus().value)
+        scheduler.runCurrent()
+        assertEquals(EventStatus.Saved, eventViewModel.getStatus().value)
+        coVerify { mockedRepository.setEvent(modifiedExistingEvent) }
     }
 }
