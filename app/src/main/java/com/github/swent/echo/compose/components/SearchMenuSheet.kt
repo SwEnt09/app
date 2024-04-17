6e16da2353f42bee247e8de5d622173f094af726
Noah Robert <noah.robert@epfl.ch>
2024-04-17 18:51:56 +0200

    reformat

--- app/src/main/java/com/github/swent/echo/compose/components/SearchMenuSheet.kt
+++ app/src/main/java/com/github/swent/echo/compose/components/SearchMenuSheet.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
