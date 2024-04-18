a90171aaf3c7479bcfdd92dd9b26aa04ed7e6faa
Jonas Sulzer <jonas@violoncello.ch>
2024-04-18 11:18:34 +0200

    🐛 FIX: manually annotate serializable names on data classes
    Signed-off-by: Jonas Sulzer <jonas@violoncello.ch>

--- app/src/main/java/com/github/swent/echo/data/model/Tag.kt
+++ app/src/main/java/com/github/swent/echo/data/model/Tag.kt
@@ -1,5 +1,6 @@
 package com.github.swent.echo.data.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Serializable data class Tag(val tagId: String, val name: String)
+@Serializable data class Tag(@SerialName("tag_id") val tagId: String, val name: String)
