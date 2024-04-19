80896b46d02793052c2d14e204538dfe9379bb4b
octogradiste <colin.berger@outlook.com>
2024-04-19 19:23:41 +0200

    Make authentication composable tests go faster by making them unittests
    Note that I could even remove some ignored tests which failed because the button was not visible in the GitHub runner emulator.

--- app/src/test/java/com/github/swent/echo/compose/authentication/LoginScreenTest.kt
+++ app/src/test/java/com/github/swent/echo/compose/authentication/LoginScreenTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.swent.echo.ui.navigation.NavigationActions
 import com.github.swent.echo.ui.navigation.Routes
 import com.github.swent.echo.viewmodels.authentication.AuthenticationState
@@ -15,10 +16,11 @@ import io.mockk.verify
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class LoginScreenTest {
 
     @get:Rule val composeTestRule = createComposeRule()
@@ -67,7 +69,6 @@ class LoginScreenTest {
         verify { navActions.navigateTo(Routes.REGISTER) }
     }
 
-    @Ignore
     @Test
     fun shouldCallNavigateToRegisterWhenRegisterButtonIsClickedInErrorState() {
         state.value = AuthenticationState.Error("Error message")
