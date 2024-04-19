c0081640bfc4941165ed1ea57a1fbd51724b2aca
octogradiste <colin.berger@outlook.com>
2024-04-19 19:23:41 +0200

    Make authentication composable tests go faster by making them unittests
    Note that I could even remove some ignored tests which failed because the button was not visible in the GitHub runner emulator.

--- app/src/test/java/com/github/swent/echo/compose/authentication/AuthenticationScreenTest.kt
+++ app/src/test/java/com/github/swent/echo/compose/authentication/AuthenticationScreenTest.kt
@@ -6,12 +6,15 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.swent.echo.viewmodels.authentication.AuthenticationState
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class AuthenticationScreenTest {
 
     companion object {
