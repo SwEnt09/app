a55a201bd6637e822893f02819a3ae3e61eb1203
octogradiste <colin.berger@outlook.com>
2024-04-23 15:15:57 +0200

    Add id to parent

--- app/src/main/java/com/github/swent/echo/data/model/Tag.kt
+++ app/src/main/java/com/github/swent/echo/data/model/Tag.kt
@@ -3,4 +3,10 @@ package com.github.swent.echo.data.model
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Serializable data class Tag(@SerialName("tag_id") val tagId: String, val name: String)
+@Serializable
+data class Tag(
+    @SerialName("tag_id") val tagId: String,
+    val name: String,
+    @SerialName("parent_id")
+    val parentId: String? = null, // TODO: We might want to remove the default value here...
+)
