bb73da31f011c3b571805836b37eba20684aaa0c
Nathan Tissières <nathan.tissieres@epfl.ch>
2024-04-24 13:13:31 +0200

    Modularization of the TopAppBar

---
+++ app/src/main/java/com/github/swent/echo/compose/components/TopAppBar.kt
@@ -0,0 +1,78 @@
+package com.github.swent.echo.compose.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import com.github.swent.echo.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(scope: CoroutineScope, drawerState: DrawerState, mode: MutableState<MapOrListMode>) {
+    // Scroll behavior for the top app bar, makes it pinned
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                stringResource(R.string.app_title),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis, // should not happen
+                fontWeight = FontWeight.SemiBold
+            )
+        },
+        navigationIcon = { // hamburger menu
+            IconButton(
+                onClick = { scope.launch { drawerState.open() } },
+                modifier = Modifier.testTag("menu_button")
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Menu to access all subpages"
+                )
+            }
+        },
+        actions = { // mode switch icon
+            IconButton(
+                onClick = {
+                    if (mode.value == MapOrListMode.MAP) {
+                        mode.value = MapOrListMode.LIST
+                    } else {
+                        mode.value = MapOrListMode.MAP
+                    }
+                },
+                modifier = Modifier.testTag("list_map_mode_button")
+            ) {
+                Icon(
+                    painter =
+                        if (mode.value == MapOrListMode.MAP) {
+                            rememberVectorPainter(image = Icons.Filled.List)
+                        } else {
+                            painterResource(id = R.drawable.map_icon)
+                        },
+                    contentDescription = "Search icon to access the search screen"
+                )
+            }
+        },
+        scrollBehavior = scrollBehavior,
+        modifier = Modifier.testTag("top_bar")
+    )
+}
