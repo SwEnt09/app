19ece3260331059f8b68666280f5b8b589e8aa1c
Jonas Sulzer <jonas@violoncello.ch>
2024-05-01 19:57:48 +0200

    📦 NEW: implement a simple offline mode where data is fetched from room if offline
    Signed-off-by: Jonas Sulzer <jonas@violoncello.ch>

--- app/src/main/java/com/github/swent/echo/di/RepositoryModule.kt
+++ app/src/main/java/com/github/swent/echo/di/RepositoryModule.kt
@@ -1,8 +1,13 @@
 package com.github.swent.echo.di
 
+import android.app.Application
+import androidx.room.Room
 import com.github.swent.echo.data.repository.Repository
 import com.github.swent.echo.data.repository.RepositoryImpl
+import com.github.swent.echo.data.repository.datasources.LocalDataSource
 import com.github.swent.echo.data.repository.datasources.RemoteDataSource
+import com.github.swent.echo.data.room.AppDatabase
+import com.github.swent.echo.data.room.RoomLocalDataSource
 import com.github.swent.echo.data.supabase.SupabaseDataSource
 import dagger.Module
 import dagger.Provides
@@ -17,16 +22,30 @@ object RepositoryModule {
 
     @Singleton
     @Provides
-    fun provideRepository(supabaseClient: SupabaseClient): Repository {
+    fun provideRepository(supabaseClient: SupabaseClient, application: Application): Repository {
         val remoteDataSource: RemoteDataSource = SupabaseDataSource(supabaseClient)
+        val localDataSource: LocalDataSource =
+            RoomLocalDataSource(
+                Room.databaseBuilder(
+                        application.applicationContext,
+                        AppDatabase::class.java,
+                        AppDatabase.APP_DATABASE_NAME
+                    )
+                    .build()
+            )
 
-        return RepositoryImpl(remoteDataSource)
+        return RepositoryImpl(
+            remoteDataSource,
+            localDataSource,
+            { true }
+        ) // TODO: use a real source for online state
     }
 
     /*
-    @Singleton
-    @Provides
-    fun provideRepository(authenticationService: AuthenticationService): Repository {
-        return SimpleRepository(authenticationService)
-    } */
+       @Singleton
+       @Provides
+       fun provideRepository(authenticationService: AuthenticationService): Repository {
+           return SimpleRepository(authenticationService)
+       }
+    */
 }
