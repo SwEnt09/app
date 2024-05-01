08baf24d4ff5f9c92c8e9a276d5a86f664686c15
Nathan Tissières <nathan.tissieres@epfl.ch>
2024-05-01 14:36:03 +0200

    HamburgerMenuDrawerSheet : sonarcloud was not happy

--- app/src/main/java/com/github/swent/echo/compose/components/HamburgerMenuDrawerSheet.kt
+++ app/src/main/java/com/github/swent/echo/compose/components/HamburgerMenuDrawerSheet.kt
@@ -12,8 +12,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.outlined.AddCircle
-import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,10 +21,6 @@ import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -50,7 +44,6 @@ fun HamburgerMenuDrawerSheet(
     profileClass: String,
     onSignOutPressed: () -> Unit
 ) {
-    var selectedItemIndex by rememberSaveable { mutableIntStateOf(0) }
     /**
      * List of navigation items to display in the hamburger menu
      *
