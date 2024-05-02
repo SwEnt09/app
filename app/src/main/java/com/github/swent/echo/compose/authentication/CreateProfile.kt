215b157a1d6e61669126be269472b5c9e46113ca
Srushti <srushti.singh@epfl.ch>
2024-05-02 12:36:32 +0200

    navigationHost changed

--- app/src/main/java/com/github/swent/echo/compose/authentication/CreateProfile.kt
+++ app/src/main/java/com/github/swent/echo/compose/authentication/CreateProfile.kt
@@ -38,6 +38,7 @@ import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -68,7 +69,6 @@ import com.github.swent.echo.ui.navigation.Routes
 import com.github.swent.echo.viewmodels.authentication.CreateProfileViewModel
 import com.github.swent.echo.viewmodels.tag.TagViewModel
 
-@SuppressLint("StateFlowValueCalledInComposition")
 @Composable
 fun ProfileCreationScreen(
     viewModel: CreateProfileViewModel,
@@ -79,7 +79,7 @@ fun ProfileCreationScreen(
     ProfileCreationUI(
         sectionList = SectionEPFL.entries,
         semList = SemesterEPFL.entries,
-        tagList = viewModel.tagList.value,
+        tagList = viewModel.tagList.collectAsState().value,
         onSave = viewModel::profilesave,
         onAdd = { dialogVisible = true },
         navAction = navAction,
--- app/src/main/java/com/github/swent/echo/compose/navigation/AppNavigationHost.kt
+++ app/src/main/java/com/github/swent/echo/compose/navigation/AppNavigationHost.kt
@@ -30,8 +30,8 @@ fun AppNavigationHost(
 
     NavHost(
         navController = navController,
-        startDestination = Routes.PROFILE_CREATION.name,
-        // startDestination = if (userIsLoggedIn) Routes.MAP.name else Routes.REGISTER.name,
+        // startDestination = Routes.PROFILE_CREATION.name,
+        startDestination = if (userIsLoggedIn) Routes.MAP.name else Routes.REGISTER.name,
     ) {
         composable(Routes.LOGIN.name) {
             LoginScreen(loginViewModel = hiltViewModel(), navActions = navActions)
