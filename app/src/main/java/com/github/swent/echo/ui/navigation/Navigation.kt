e31fc2b4fad4070f5d150a79e18239ccbf543b3e
octogradiste <colin.berger@outlook.com>
2024-05-02 19:47:35 +0200

    Add support navigating with arguments

--- app/src/main/java/com/github/swent/echo/ui/navigation/Navigation.kt
+++ app/src/main/java/com/github/swent/echo/ui/navigation/Navigation.kt
@@ -3,20 +3,36 @@ package com.github.swent.echo.ui.navigation
 import androidx.navigation.NavHostController
 
 /** The routes to the screens of the app */
-enum class Routes {
-    LOGIN,
-    REGISTER,
-    MAP,
-    CREATE_EVENT,
-    EDIT_EVENT
+data class Routes(val name: String) {
+
+    /** The different routes available */
+    companion object {
+        val LOGIN = Routes("login")
+        val REGISTER = Routes("register")
+        val MAP = Routes("map")
+        val CREATE_EVENT = Routes("create_event")
+        val EDIT_EVENT = RoutesBuilder("edit_event", "eventId")
+    }
 }
 
 /**
- * This class reprensent the different navigation actions
+ * This class represent a route with an argument. Use [build] to create build the route with an
+ * argument.
+ */
+data class RoutesBuilder(private val route: String, private val argName: String) {
+    val name: String = "$route/{$argName}"
+
+    fun build(arg: String): Routes {
+        return Routes("$route/$arg")
+    }
+}
+
+/**
+ * This class represent the different navigation actions
  *
  * @param navController the navigation controller
  */
-class NavigationActions(val navController: NavHostController) {
+class NavigationActions(private val navController: NavHostController) {
 
     // navigate to one of the routes available in Routes
     fun navigateTo(route: Routes) {
