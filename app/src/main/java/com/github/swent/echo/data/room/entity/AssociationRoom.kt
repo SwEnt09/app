d87824b41d1b946adea16e91b098b54e04ba8cde
octogradiste <colin.berger@outlook.com>
2024-05-04 20:30:01 +0200

    Add timestamp + specify foreign key relation

--- app/src/main/java/com/github/swent/echo/data/room/entity/AssociationRoom.kt
+++ app/src/main/java/com/github/swent/echo/data/room/entity/AssociationRoom.kt
@@ -3,12 +3,15 @@ package com.github.swent.echo.data.room.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.github.swent.echo.data.model.Association
+import java.time.ZonedDateTime
 
 @Entity
 data class AssociationRoom(
     @PrimaryKey val associationId: String,
     val name: String,
-    val description: String
+    val description: String,
+    /** The time of the last update in seconds */
+    val timestamp: Long = ZonedDateTime.now().toEpochSecond(),
 ) {
     constructor(
         association: Association
--- app/src/main/java/com/github/swent/echo/data/room/entity/TagRoom.kt
+++ app/src/main/java/com/github/swent/echo/data/room/entity/TagRoom.kt
@@ -3,12 +3,15 @@ package com.github.swent.echo.data.room.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.github.swent.echo.data.model.Tag
+import java.time.ZonedDateTime
 
 @Entity
 data class TagRoom(
     @PrimaryKey val tagId: String,
     val parentId: String?,
     val name: String,
+    /** The time of the last update in seconds */
+    val timestamp: Long = ZonedDateTime.now().toEpochSecond(),
 ) {
     constructor(tag: Tag) : this(tag.tagId, tag.parentId, tag.name)
 
