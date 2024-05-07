8bd333e067951d0087e52139ea9d3e12d31ce82d
yoan <yoan@laptop>
2024-05-07 10:19:45 +0200

    add tests for association list screen

--- app/src/main/java/com/github/swent/echo/compose/association/AssociationListScreen.kt
+++ app/src/main/java/com/github/swent/echo/compose/association/AssociationListScreen.kt
@@ -3,7 +3,6 @@ package com.github.swent.echo.compose.association
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
