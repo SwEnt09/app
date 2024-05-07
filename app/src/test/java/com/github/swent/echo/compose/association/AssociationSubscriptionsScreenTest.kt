0315d64f25cbea38d5a78f24f72c988420985665
yoan <yoan@laptop>
2024-05-07 11:50:09 +0200

    add tests for the association subscriptions screen

---
+++ app/src/test/java/com/github/swent/echo/compose/association/AssociationSubscriptionsScreenTest.kt
@@ -0,0 +1,54 @@
+package com.github.swent.echo.compose.association
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.swent.echo.data.model.Association
+import com.github.swent.echo.data.model.UserProfile
+import com.github.swent.echo.ui.navigation.NavigationActions
+import com.github.swent.echo.viewmodels.user.UserProfileViewModel
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AssociationSubscriptionsScreenTest {
+
+    @get:Rule val composeTestRule = createComposeRule()
+
+    private val mockedNavActions = mockk<NavigationActions>(relaxed = true)
+    private val userProfileViewModel = mockk<UserProfileViewModel>(relaxed = true)
+    private val userProfile =
+        UserProfile.EMPTY.copy(
+            associationsSubscriptions = setOf(Association("testAI", "testAN", "testAD"))
+        )
+
+    @Before
+    fun init() {
+        composeTestRule.setContent {
+            every { userProfileViewModel.userProfile } returns MutableStateFlow(userProfile)
+            AssociationSubscriptionsScreen(
+                userProfileViewModel = userProfileViewModel,
+                navigationActions = mockedNavActions
+            )
+        }
+    }
+
+    @Test
+    fun goBackButtonShouldGoBack() {
+        composeTestRule.onNodeWithTag("Back-button").performClick()
+        verify { mockedNavActions.goBack() }
+    }
+
+    @Test
+    fun unfollowButtonShouldChangeUserProfile() {
+        composeTestRule.onNodeWithTag("testAI-action-button").performClick()
+        verify { userProfileViewModel.setUserProfile(UserProfile.EMPTY) }
+    }
+}
