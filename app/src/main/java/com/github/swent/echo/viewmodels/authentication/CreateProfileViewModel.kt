68b1f860de8e53f29a10b1d7b8e0d0dc1daa32e6
Srushti <srushti.singh@epfl.ch>
2024-05-09 08:33:53 +0200

    bugs fixed in profile creation composable

--- app/src/main/java/com/github/swent/echo/viewmodels/authentication/CreateProfileViewModel.kt
+++ app/src/main/java/com/github/swent/echo/viewmodels/authentication/CreateProfileViewModel.kt
@@ -2,6 +2,7 @@ package com.github.swent.echo.viewmodels.authentication
 
 import androidx.lifecycle.ViewModel
 import com.github.swent.echo.authentication.AuthenticationService
+import com.github.swent.echo.data.model.Association
 import com.github.swent.echo.data.model.SectionEPFL
 import com.github.swent.echo.data.model.SemesterEPFL
 import com.github.swent.echo.data.model.Tag
@@ -36,6 +37,12 @@ constructor(
     private val _tagList = MutableStateFlow<Set<Tag>>(setOf())
     val tagList = _tagList.asStateFlow()
 
+    private val _committeeMember = MutableStateFlow<Set<Association>>(setOf())
+    val committeeMember = _committeeMember.asStateFlow()
+
+    private val _associationSubscriptions = MutableStateFlow<Set<Association>>(setOf())
+    val associationSubscriptions = _associationSubscriptions.asStateFlow()
+
     private val _errorMessage = MutableStateFlow<String?>(null)
     val errorMessage = _errorMessage.asStateFlow()
 
@@ -57,6 +64,9 @@ constructor(
         _selectedSemester.value = semester
     }
 
+    //TO ASK: how does it work for the associations and committee members? Of course they cannot be set by the user, but how do they get set?
+    //TO ASK: how to display the associations a user is subscribe to?
+
     // TODO: Save profile button => go to the home screen after saving the profile.
     fun profilesave() {
 
@@ -68,9 +78,20 @@ constructor(
         } else {
             // TODO: this is the logic for creating a userProfile, changes are required in the
             // getUserProfile function so that it returns null if a user has not yet created a
-
+            // profile.
             // TODO: show loading animation while saving the profile
+
             runBlocking {
+                val userProfile = repository.getUserProfile(userId)
+                if (userProfile != null) {
+                    _firstName.value = userProfile.name.split(" ")[0]
+                    _lastName.value = userProfile.name.split(" ")[1]
+                    _selectedSemester.value = userProfile.semester as SemesterEPFL?
+                    _selectedSection.value = userProfile.section as SectionEPFL?
+                    _tagList.value = userProfile.tags
+                    _committeeMember.value = userProfile.committeeMember
+                    _associationSubscriptions.value = userProfile.associationsSubscriptions
+                }
                 repository.setUserProfile(
                     UserProfile(
                         userId,
@@ -78,8 +99,8 @@ constructor(
                         semester = _selectedSemester.value,
                         section = _selectedSection.value,
                         tags = _tagList.value,
-                        committeeMember = setOf(),
-                        associationsSubscriptions = setOf()
+                        committeeMember = _committeeMember.value,
+                        associationsSubscriptions = _associationSubscriptions.value
                     )
                 )
             }
@@ -90,5 +111,9 @@ constructor(
     // Add tag button
     fun addTag(tag: Tag) {
         _tagList.value += tag
+    }
+
+    fun removeTag(tag: Tag) {
+        _tagList.value -= tag
     }
 }
