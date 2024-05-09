e5806b7b3a67c4d0d62ae8971b6a192f6c99fdcb
yoan <yoan@laptop>
2024-05-09 14:30:04 +0200

    remove description and add snackbar to revert action

--- app/src/main/java/com/github/swent/echo/compose/association/AssociationListScreen.kt
+++ app/src/main/java/com/github/swent/echo/compose/association/AssociationListScreen.kt
@@ -10,8 +10,13 @@ import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -20,7 +25,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.github.swent.echo.R
 import com.github.swent.echo.compose.event.EventTitleAndBackButton
@@ -34,25 +38,56 @@ fun AssociationListScreen(
     associationList: List<Association>,
     hasActionButton: Boolean = false,
     actionButtonName: String = "",
+    actionSnackbarMessage: String = "",
+    actionSnackbarUndoMessage: String = "",
     onActionButtonClicked: (association: Association) -> Unit = {},
-    displayDescription: Boolean
+    onUndoActionButtonClicked: (association: Association) -> Unit = {},
+    onAssociationClicked: (association: Association) -> Unit = {}
 ) {
-    Scaffold(topBar = { EventTitleAndBackButton(title, onBackButtonClicked) }) { paddingValues ->
+    val snackBarHostState by remember { mutableStateOf(SnackbarHostState()) }
+    var lastActionAssociation by remember { mutableStateOf(Association.EMPTY) }
+    if (lastActionAssociation != Association.EMPTY) {
+        LaunchedEffect(lastActionAssociation) {
+            val result =
+                snackBarHostState.showSnackbar(
+                    actionSnackbarMessage + lastActionAssociation.name,
+                    actionSnackbarUndoMessage,
+                    true,
+                    SnackbarDuration.Long
+                )
+            when (result) {
+                SnackbarResult.ActionPerformed -> {
+                    onUndoActionButtonClicked(lastActionAssociation)
+                }
+                SnackbarResult.Dismissed -> {
+                    // do nothing
+                }
+            }
+        }
+    }
+    Scaffold(
+        topBar = { EventTitleAndBackButton(title, onBackButtonClicked) },
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState, modifier = Modifier.testTag("snackbar"))
+        }
+    ) { paddingValues ->
         LazyColumn(modifier = Modifier.padding(paddingValues)) {
             items(associationList.size) { index ->
                 AssociationListElement(
                     association = associationList[index],
                     hasActionButton = hasActionButton,
                     actionButtonName = actionButtonName,
-                    onActionButtonClicked = { onActionButtonClicked(associationList[index]) },
-                    displayDescription = displayDescription
+                    onActionButtonClicked = {
+                        onActionButtonClicked(associationList[index])
+                        lastActionAssociation = associationList[index]
+                    },
+                    onAssociationClicked = { onAssociationClicked(associationList[index]) }
                 )
             }
         }
     }
 }
 
-const val SHORT_DESCRIPTION_LINES = 2
 /** An element in the association list */
 @Composable
 fun AssociationListElement(
@@ -60,14 +95,13 @@ fun AssociationListElement(
     hasActionButton: Boolean,
     actionButtonName: String,
     onActionButtonClicked: () -> Unit,
-    displayDescription: Boolean
+    onAssociationClicked: () -> Unit
 ) {
-    var expanded by remember { mutableStateOf(false) }
     ListItem(
         modifier =
             Modifier.padding(vertical = 7.dp, horizontal = 15.dp)
                 .clip(RoundedCornerShape(5.dp))
-                .clickable(enabled = displayDescription) { expanded = !expanded },
+                .clickable { onAssociationClicked() },
         overlineContent = { Spacer(modifier = Modifier) }, // used to align the icon and the button
         headlineContent = {
             Text(
@@ -75,22 +109,6 @@ fun AssociationListElement(
                 modifier = Modifier.testTag("${association.associationId}-name"),
                 style = MaterialTheme.typography.titleMedium
             )
-        },
-        supportingContent = {
-            if (displayDescription) {
-                Text(
-                    modifier = Modifier.testTag("${association.associationId}-description"),
-                    text = association.description,
-                    style = MaterialTheme.typography.bodyMedium,
-                    maxLines =
-                        if (expanded) {
-                            Int.MAX_VALUE
-                        } else {
-                            SHORT_DESCRIPTION_LINES
-                        },
-                    overflow = TextOverflow.Ellipsis
-                )
-            }
         },
         leadingContent = { Icon(painter = painterResource(R.drawable.confirmed), null) },
         tonalElevation = 5.dp,
--- app/src/main/java/com/github/swent/echo/compose/association/AssociationSubscriptionsScreen.kt
+++ app/src/main/java/com/github/swent/echo/compose/association/AssociationSubscriptionsScreen.kt
@@ -2,6 +2,8 @@ package com.github.swent.echo.compose.association
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import com.github.swent.echo.R
 import com.github.swent.echo.ui.navigation.NavigationActions
@@ -13,14 +15,18 @@ fun AssociationSubscriptionsScreen(
     userProfileViewModel: UserProfileViewModel,
     navigationActions: NavigationActions
 ) {
-    val associationsSubscriptions =
-        userProfileViewModel.userProfile.collectAsState().value.associationsSubscriptions.toList()
+    val userProfile by userProfileViewModel.userProfile.collectAsState()
+    val associationsSubscriptions = userProfile.associationsSubscriptions.toList()
     AssociationListScreen(
         title = stringResource(R.string.association_subscription_screen_title),
         onBackButtonClicked = { navigationActions.goBack() },
         associationList = associationsSubscriptions,
         hasActionButton = true,
         actionButtonName = stringResource(R.string.association_subscription_screen_unfollow),
+        actionSnackbarMessage =
+            stringResource(R.string.association_subscription_screen_snackbar_message),
+        actionSnackbarUndoMessage =
+            stringResource(R.string.association_subscription_screen_snackbar_undo),
         onActionButtonClicked = {
             val newAssociationSubscriptions =
                 associationsSubscriptions.filter { association -> association != it }
@@ -30,6 +36,15 @@ fun AssociationSubscriptionsScreen(
                 )
             )
         },
-        displayDescription = true
+        onUndoActionButtonClicked = {
+            userProfileViewModel.setUserProfile(
+                userProfileViewModel.userProfile.value.copy(
+                    associationsSubscriptions = (associationsSubscriptions + it).toSet()
+                )
+            )
+        },
+        onAssociationClicked = {
+            // TODO: navigate to association detail screen
+        }
     )
 }
