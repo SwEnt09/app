a2fa44b3febfbbfbdfab856cc2078942cb3209ce
octogradiste <colin.berger@outlook.com>
2024-05-10 16:17:59 +0200

    Make the MainActivityTest pass.
    I had to add more logic into the authentication view model, so that we can redirect to the correct route.

--- app/src/main/java/com/github/swent/echo/viewmodels/authentication/AbstractAuthenticationViewModel.kt
+++ app/src/main/java/com/github/swent/echo/viewmodels/authentication/AbstractAuthenticationViewModel.kt
@@ -2,14 +2,19 @@ package com.github.swent.echo.viewmodels.authentication
 
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.github.swent.echo.authentication.AuthenticationService
+import com.github.swent.echo.data.repository.Repository
+import com.github.swent.echo.ui.navigation.Routes
 import io.github.jan.supabase.compose.auth.composable.NativeSignInResult
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 /** Provides Google sign in functionality. */
 abstract class AbstractAuthenticationViewModel : ViewModel() {
     protected abstract val auth: AuthenticationService
+    protected abstract val repository: Repository
 
     protected val _state = MutableStateFlow<AuthenticationState>(AuthenticationState.SignedOut)
 
@@ -39,7 +44,25 @@ abstract class AbstractAuthenticationViewModel : ViewModel() {
                 _state.value = AuthenticationState.Error("Google sign in failed.")
             is NativeSignInResult.NetworkError ->
                 _state.value = AuthenticationState.Error("A network error occurred.")
-            is NativeSignInResult.Success -> _state.value = AuthenticationState.SignedIn
+            is NativeSignInResult.Success ->
+                viewModelScope.launch {
+                    _state.value = AuthenticationState.SignedIn(getNextRoute())
+                }
+        }
+    }
+
+    /**
+     * Returns the next route depending on the value of the user profile. If the profile is null, it
+     * returns [Routes.PROFILE_CREATION] and [Routes.MAP] otherwise. Note you should only call this
+     * function if the user has sucessfully logged as as it makes the assumption that the user id is
+     * not null.
+     */
+    protected suspend fun getNextRoute(): Routes {
+        val userId = auth.getCurrentUserID()!!
+        return if (repository.getUserProfile(userId) == null) {
+            Routes.PROFILE_CREATION
+        } else {
+            Routes.MAP
         }
     }
 }
