921b5d29a877690e27aa8efedd0b9983f8988e4b
octogradiste <colin.berger@outlook.com>
2024-05-10 13:51:14 +0200

    Add loading screen

---
+++ app/src/main/java/com/github/swent/echo/compose/authentication/LoadingScreen.kt
@@ -0,0 +1,27 @@
+package com.github.swent.echo.compose.authentication
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingScreen() {
+    Column(
+        modifier = Modifier.testTag("LoadingScreen"),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.width(64.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+    }
+}
