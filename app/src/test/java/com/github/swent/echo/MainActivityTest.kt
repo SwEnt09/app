bd472f7227dbab8ba189336a3e7503ab42e29f49
octogradiste <colin.berger@outlook.com>
2024-05-11 21:41:02 +0200

    Move MainActivityTest

--- app/src/test/java/com/github/swent/echo/MainActivityTest.kt
+++ app/src/test/java/com/github/swent/echo/MainActivityTest.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.test.performScrollTo
 import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.performTextReplacement
 import androidx.compose.ui.test.requestFocus
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.swent.echo.authentication.AuthenticationService
 import com.github.swent.echo.data.SAMPLE_EVENTS
 import com.github.swent.echo.data.model.SectionEPFL
@@ -27,8 +28,10 @@ import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
 @HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
     companion object {
