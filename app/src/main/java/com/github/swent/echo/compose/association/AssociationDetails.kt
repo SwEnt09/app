a9619aa6e1434b00f28444732c252395eb8ecf89
Nathan Tissières <nathan.tissieres@epfl.ch>
2024-05-15 16:04:29 +0200

    Making pages tests friendly

--- app/src/main/java/com/github/swent/echo/compose/association/AssociationDetails.kt
+++ app/src/main/java/com/github/swent/echo/compose/association/AssociationDetails.kt
@@ -1,6 +1,9 @@
 package com.github.swent.echo.compose.association
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import com.github.swent.echo.data.model.Association
 import com.github.swent.echo.data.model.Event
 
@@ -10,4 +13,6 @@ fun AssociationDetails(
     association: Association,
     isFollowed: Boolean,
     events: List<Event>
-) {}
+) {
+    Text("Association Details", modifier = Modifier.testTag("association_details"))
+}
--- app/src/main/java/com/github/swent/echo/compose/association/AssociationSearch.kt
+++ app/src/main/java/com/github/swent/echo/compose/association/AssociationSearch.kt
@@ -1,10 +1,15 @@
 package com.github.swent.echo.compose.association
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import com.github.swent.echo.data.model.Association
 import com.github.swent.echo.viewmodels.association.AssociationPage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AssociationSearch(goTo: (AssociationPage) -> Unit, associations: List<Association>) {}
+fun AssociationSearch(goTo: (AssociationPage) -> Unit, associations: List<Association>) {
+    Text("Association Search", modifier = Modifier.testTag("association_search"))
+}
