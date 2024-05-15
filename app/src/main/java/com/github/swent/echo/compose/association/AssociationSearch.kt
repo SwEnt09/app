eca7927eece7cff07f3dc0968c8be8dc85e19603
Nathan Tissières <nathan.tissieres@epfl.ch>
2024-05-15 19:57:13 +0200

    AssociationSearch

--- app/src/main/java/com/github/swent/echo/compose/association/AssociationSearch.kt
+++ app/src/main/java/com/github/swent/echo/compose/association/AssociationSearch.kt
@@ -1,15 +1,23 @@
 package com.github.swent.echo.compose.association
 
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import com.github.swent.echo.data.model.Association
 import com.github.swent.echo.viewmodels.association.AssociationPage
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AssociationSearch(goTo: (AssociationPage) -> Unit, associations: List<Association>) {
-    Text("Association Search", modifier = Modifier.testTag("association_search"))
+    AssociationListScreen(
+        associations,
+        onAssociationClicked = {
+            val nextPage = AssociationPage.DETAILS
+            nextPage.association = it
+            goTo(nextPage)
+        },
+        onRowClicked = {
+            val nextPage = AssociationPage.DETAILS
+            nextPage.association = it
+            goTo(nextPage)
+        },
+        eventsFilter = emptyList()
+    )
 }
