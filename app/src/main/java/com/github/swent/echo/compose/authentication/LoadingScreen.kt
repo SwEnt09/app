0bb0430ef029605a2e75fcdef03073a87b1672df
octogradiste <colin.berger@outlook.com>
2024-05-16 19:01:40 +0200

    Center the loading animation + wrap inside a scaffold

--- app/src/main/java/com/github/swent/echo/compose/authentication/LoadingScreen.kt
+++ app/src/main/java/com/github/swent/echo/compose/authentication/LoadingScreen.kt
@@ -2,9 +2,12 @@ package com.github.swent.echo.compose.authentication
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,15 +16,17 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun LoadingScreen() {
-    Column(
-        modifier = Modifier.testTag("LoadingScreen"),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-    ) {
-        CircularProgressIndicator(
-            modifier = Modifier.width(64.dp),
-            color = MaterialTheme.colorScheme.secondary,
-            trackColor = MaterialTheme.colorScheme.surfaceVariant,
-        )
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier.testTag("LoadingScreen").fillMaxSize().padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier.width(64.dp),
+                color = MaterialTheme.colorScheme.secondary,
+                trackColor = MaterialTheme.colorScheme.surfaceVariant,
+            )
+        }
     }
 }
