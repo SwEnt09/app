cce4c3149da936c9f76bc5419fdf38f8f1593d55
Nathan Tissières <nathan.tissieres@epfl.ch>
2024-05-23 19:40:21 +0200

    SearchBar : Creating a reusable SearchBar

---
+++ app/src/main/java/com/github/swent/echo/compose/components/SearchBar.kt
@@ -0,0 +1,38 @@
+package com.github.swent.echo.compose.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SearchBar(title: String, searched: String, onSearchChanged: (String) -> Unit) {
+    val paddingHorizontal = 10.dp
+    val cornerRadius = 10.dp
+    OutlinedTextField(
+        label = { Text(title) },
+        value = searched,
+        onValueChange = onSearchChanged,
+        trailingIcon = {
+            if(searched.isBlank()){
+                Icon(Icons.Outlined.Search, "search")
+            }else{
+                IconButton(
+                    onClick = { onSearchChanged("") },
+                    content = { Icon(Icons.Outlined.Close, "close") }
+                )
+            }
+        },
+        modifier = Modifier.fillMaxWidth().padding(horizontal = paddingHorizontal),
+        shape = RoundedCornerShape(cornerRadius)
+    )
+}
