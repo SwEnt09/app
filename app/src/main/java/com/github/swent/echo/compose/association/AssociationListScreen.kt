1365ff09a4b0cd52d13bd9205151bfda729cf700
Nathan Tissières <nathan.tissieres@epfl.ch>
2024-05-23 19:46:07 +0200

    AssociationListScreen : Updating for the new UI

--- app/src/main/java/com/github/swent/echo/compose/association/AssociationListScreen.kt
+++ app/src/main/java/com/github/swent/echo/compose/association/AssociationListScreen.kt
@@ -4,14 +4,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.style.TextAlign
@@ -23,16 +18,12 @@ import com.github.swent.echo.data.model.Association
 fun AssociationListScreen(
     associationList: List<Association>,
     onRowClicked: (Association) -> Unit = {},
-    onAssociationClicked: (Association) -> Unit = {},
-    eventsFilter: List<Association>
 ) {
     LazyColumn(modifier = Modifier.testTag("association_list_screen")) {
         items(associationList.size) { index ->
             AssociationListElement(
                 association = associationList[index],
-                onRowClicked = { onRowClicked(it) },
-                onAssociationClicked = { onAssociationClicked(it) },
-                eventsFilter
+                onRowClicked = { onRowClicked(it) }
             )
         }
     }
@@ -43,12 +34,9 @@ fun AssociationListScreen(
 fun AssociationListElement(
     association: Association,
     onRowClicked: (Association) -> Unit,
-    onAssociationClicked: (Association) -> Unit,
-    eventsFilter: List<Association>
 ) {
     val boxInsidePadding = 5.dp
     val tonalElevation = 5.dp
-    val paddingSelectedIcon = 5.dp
     ListItem(
         modifier =
             Modifier.clickable { onRowClicked(association) }
@@ -56,26 +44,11 @@ fun AssociationListElement(
         overlineContent = { Spacer(modifier = Modifier) }, // used to align the icon and the button
         headlineContent = {},
         leadingContent = {
-            TextButton(
-                onClick = { onAssociationClicked(association) },
-                modifier = Modifier.testTag("association_name_button_${association.name}")
-            ) {
-                Text(
-                    text = association.name,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.align(Alignment.CenterVertically).padding(boxInsidePadding)
-                )
-            }
-        },
-        trailingContent = {
-            if (eventsFilter.contains(association)) {
-                Icon(
-                    Icons.Filled.Done,
-                    "Selected",
-                    Modifier.padding(paddingSelectedIcon)
-                        .testTag("selected_icon_${association.name}")
-                )
-            }
+            Text(
+                text = association.name,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(boxInsidePadding).testTag("association_name_button_${association.name}")
+            )
         },
         tonalElevation = tonalElevation,
     )
