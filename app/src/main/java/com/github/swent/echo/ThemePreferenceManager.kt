dbbdfedeccac7305e0532921bc88c43823767943
Srushti <srushti.singh@epfl.ch>
2024-05-27 16:29:21 +0200

    requested changes made

--- app/src/main/java/com/github/swent/echo/ThemePreferenceManager.kt
+++ app/src/main/java/com/github/swent/echo/ThemePreferenceManager.kt
@@ -1,6 +1,7 @@
 package com.github.swent.echo
 
 import android.content.Context
+import android.content.res.Configuration
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
@@ -17,9 +18,17 @@ class ThemePreferenceManager @Inject constructor(context: Context) {
 
     val theme: Flow<AppTheme> =
         dataStore.data.map { preferences ->
-            val themeString = preferences[THEME_KEY] ?: AppTheme.MODE_NIGHT.name
+            val themeString = preferences[THEME_KEY] ?: getSystemDefaultTheme(context).name
             AppTheme.valueOf(themeString)
         }
+
+    private fun getSystemDefaultTheme(context: Context): AppTheme {
+        return if (context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES) {
+            AppTheme.MODE_NIGHT
+        } else {
+            AppTheme.MODE_DAY
+        }
+    }
 
     suspend fun setTheme(theme: AppTheme) {
         dataStore.edit { preferences -> preferences[THEME_KEY] = theme.name }
