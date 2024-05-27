e86788f7bc4ec473845a8f5f2bf11b8b43d4cbab
octogradiste <colin.berger@outlook.com>
2024-05-27 22:23:57 +0200

    Define general interface to save bytes to the cache

---
+++ app/src/main/java/com/github/swent/echo/data/repository/datasources/FileCache.kt
@@ -0,0 +1,27 @@
+package com.github.swent.echo.data.repository.datasources
+
+/** Interface for file caching. */
+interface FileCache {
+
+    /**
+     * Get the file content by name.
+     *
+     * @param name The name of the file to get.
+     */
+    suspend fun get(name: String): ByteArray?
+
+    /**
+     * Set the file content by name.
+     *
+     * @param name The name of the file to set.
+     * @param content The content of the file to set.
+     */
+    suspend fun set(name: String, content: ByteArray)
+
+    /**
+     * Delete the file by name.
+     *
+     * @param name The name of the file to delete.
+     */
+    suspend fun delete(name: String)
+}
