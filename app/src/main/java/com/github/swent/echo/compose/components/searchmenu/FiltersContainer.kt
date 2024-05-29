eba24567472afff3f2ec9bd614254a05c8fb82bb
Nathan Tissières <nathan.tissieres@epfl.ch>
2024-05-29 11:36:13 +0200

    FiltersContainer : adapting it to the new dropdown

--- app/src/main/java/com/github/swent/echo/compose/components/searchmenu/FiltersContainer.kt
+++ app/src/main/java/com/github/swent/echo/compose/components/searchmenu/FiltersContainer.kt
@@ -1,6 +1,6 @@
 package com.github.swent.echo.compose.components.searchmenu
 
-import com.github.swent.echo.R
+import com.github.swent.echo.viewmodels.SortBy
 
 data class FiltersContainer(
     var searchEntry: String,
@@ -12,27 +12,5 @@ data class FiltersContainer(
     var fullChecked: Boolean,
     var from: Float,
     var to: Float,
-    var sortBy: SortBy,
+    var sortBy: SortBy?
 )
-
-// Enum class for the different states of the sort by filter
-enum class SortBy(val stringKey: String) {
-    NONE("filters_container_sort_by_none"),
-    DATE_ASC("filters_container_sort_by_date_asc"),
-    DATE_DESC("filters_container_sort_by_date_desc"),
-    DISTANCE_ASC("filters_container_sort_by_distance_asc"),
-    DISTANCE_DESC("filters_container_sort_by_distance_desc"),
-}
-
-// Function to get the string resource for the sort by filter
-fun stringResourceSortBy(key: String): Int {
-    return when (key) {
-        "filters_container_sort_by_none" -> R.string.filters_container_sort_by_none
-        "filters_container_sort_by_date_asc" -> R.string.filters_container_sort_by_date_asc
-        "filters_container_sort_by_date_desc" -> R.string.filters_container_sort_by_date_desc
-        "filters_container_sort_by_distance_asc" -> R.string.filters_container_sort_by_distance_asc
-        "filters_container_sort_by_distance_desc" ->
-            R.string.filters_container_sort_by_distance_desc
-        else -> throw IllegalArgumentException("Invalid string key")
-    }
-}
