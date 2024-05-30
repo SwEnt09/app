9f37d6d94fefbc8a7a7dfec6a93ee85bdc985eee
yoan <yoan@laptop>
2024-05-30 11:07:34 +0200

    add comments to the basic data classes

--- app/src/main/java/com/github/swent/echo/data/model/Association.kt
+++ app/src/main/java/com/github/swent/echo/data/model/Association.kt
@@ -1,5 +1,14 @@
 package com.github.swent.echo.data.model
 
+/**
+ * The association data class.
+ *
+ * @property associationId the unique id of the association
+ * @property name the name of the association
+ * @property description the description of the association
+ * @property url the link to the association web page
+ * @property relatedTags the tags related to the association
+ */
 data class Association(
     val associationId: String,
     val name: String,
--- app/src/main/java/com/github/swent/echo/data/model/UserProfile.kt
+++ app/src/main/java/com/github/swent/echo/data/model/UserProfile.kt
@@ -1,5 +1,16 @@
 package com.github.swent.echo.data.model
 
+/**
+ * The user profile data class.
+ *
+ * @property userId the unique id of the user
+ * @property name the name of the user
+ * @property semester the semester of the user
+ * @property section the section of the user
+ * @property tags the tags related to the user
+ * @property committeeMember the list of associations where the user is a committee member
+ * @property associationsSubscriptions the list of association subscriptions of the user
+ */
 data class UserProfile(
     val userId: String,
     val name: String,
