bbe48bc0396587b8ac3076de3ee001da603ea0e7
alejandrocalles <gabriel.jimenezcalles@epfl.ch>
2024-06-01 20:11:36 +0200

    Add documentation.

--- app/src/main/java/com/github/swent/echo/connectivity/GPSService.kt
+++ app/src/main/java/com/github/swent/echo/connectivity/GPSService.kt
@@ -3,11 +3,15 @@ package com.github.swent.echo.connectivity
 import com.mapbox.mapboxsdk.geometry.LatLng
 import kotlinx.coroutines.flow.StateFlow
 
-/** A GPS service. Allows the location to be accessed either as */
+/** A service that provides the user's current location if it's accessible. */
 interface GPSService {
+
+    /** The user location as a state flow. */
     val userLocation: StateFlow<LatLng?>
 
+    /** The current user location. */
     fun currentUserLocation(): LatLng?
 
+    /** Force the service to refresh the user location */
     fun refreshUserLocation()
 }
