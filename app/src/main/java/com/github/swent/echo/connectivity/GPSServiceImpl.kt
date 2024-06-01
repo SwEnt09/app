3afe5d3daeb20f87b0ae091fa3be4cd47c2536ec
alejandrocalles <gabriel.jimenezcalles@epfl.ch>
2024-06-01 20:11:36 +0200

    Add documentation.

--- app/src/main/java/com/github/swent/echo/connectivity/GPSServiceImpl.kt
+++ app/src/main/java/com/github/swent/echo/connectivity/GPSServiceImpl.kt
@@ -12,12 +12,22 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
+/**
+ * A simple implementation of a [GPSService]
+ *
+ * @param context The context of the application that's using this service.
+ */
 class GPSServiceImpl(val context: Context) : GPSService {
     private val locationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+
+    // This manager allows to check whether the location is disabled in settings.
     private val locationManager: LocationManager? =
         getSystemService(context, LocationManager::class.java)
+
+    // User's last known location
     private var _location = MutableStateFlow<LatLng?>(null)
 
+    /** Updates the last known location. */
     private fun getLocation() {
         if (
             LOCATION_PERMISSIONS.any {
@@ -32,7 +42,9 @@ class GPSServiceImpl(val context: Context) : GPSService {
                         this.let { location -> LatLng(location.latitude, location.longitude) }
                     )
                 }
-                // Request again if null, unless location is turned off in settings
+                // Request again if null, unless location is turned off in settings.
+                // Even if this process has permissions to access the location, as
+                // long as it's disabled in settings, the listener will return null.
                 locationManager?.apply {
                     // TODO maybe also check for NETWORK_PROVIDER
                     if (isProviderEnabled(LocationManager.GPS_PROVIDER)) {
