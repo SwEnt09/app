89e7d99256effaf60efcd3af255301712c5cc2de
Jonas Sulzer <jonas@violoncello.ch>
2024-06-02 09:39:39 +0200

    📖 DOC: comments on data classes
    Signed-off-by: Jonas Sulzer <jonas@violoncello.ch>

--- app/src/main/java/com/github/swent/echo/data/model/AssociationHeader.kt
+++ app/src/main/java/com/github/swent/echo/data/model/AssociationHeader.kt
@@ -3,6 +3,13 @@ package com.github.swent.echo.data.model
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * A simplified version of the Association data class, used to embed into other data classes
+ * avoiding to embed the complete heavy Association data class.
+ *
+ * @property associationId the unique id of the association
+ * @property name the name of the association
+ */
 @Serializable
 data class AssociationHeader(
     @SerialName("association_id") val associationId: String,
--- app/src/main/java/com/github/swent/echo/data/model/EventCreator.kt
+++ app/src/main/java/com/github/swent/echo/data/model/EventCreator.kt
@@ -4,7 +4,8 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /**
- * A creator of an event.
+ * A creator of an event. A simplified version of the Event data class, used to embed into other
+ * data classes avoiding to embed the complete heavy Event data class.
  *
  * @property userId The ID of the user who created the event.
  * @property name The name of the creator. This same name as in the [UserProfile] corresponding to
