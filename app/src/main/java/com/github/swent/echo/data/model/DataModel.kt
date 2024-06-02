8fa66b95acbe8092e3ac48ca03ed0e98a41ef371
Jonas Sulzer <jonas@violoncello.ch>
2024-06-02 09:39:39 +0200

    📖 DOC: comments on data classes
    Signed-off-by: Jonas Sulzer <jonas@violoncello.ch>

--- app/src/main/java/com/github/swent/echo/data/model/DataModel.kt
+++ app/src/main/java/com/github/swent/echo/data/model/DataModel.kt
@@ -1,5 +1,9 @@
 package com.github.swent.echo.data.model
 
+/**
+ * Supertype for other data classes having a unique id. Provides a unified way to get the unique id
+ * of the object.
+ */
 sealed class DataModel {
     fun getId(): String =
         when (this) {
